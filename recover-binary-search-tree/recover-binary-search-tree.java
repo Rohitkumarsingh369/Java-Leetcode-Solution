15a6a4130e5df13db24d595f6bb820150d807686
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-01-16 12:07:32 +0530

    Time: 1 ms (100.00%), Space: 39.1 MB (84.97%) - LeetHub

---
+++ recover-binary-search-tree/recover-binary-search-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    static TreeNode prev,first,last,mid;
+    public void recoverTree(TreeNode root) {
+        prev=first=last=mid=null;
+        swapcheck(root);
+        
+        // Fix (or correct) the tree 
+        if( first != null && last != null ) 
+        { 
+            int temp = first.val; 
+            first.val = last.val; 
+            last.val = temp;  
+        } 
+        // Adjacent nodes swapped 
+        else if( first != null && mid != null )  
+        { 
+            int temp = first.val; 
+            first.val = mid.val; 
+            mid.val = temp; 
+        } 
+    }
+    static void swapcheck(TreeNode root) {
+        if(root!=null) {
+            swapcheck(root.left);
+            
+            if(prev!=null && root.val<prev.val) {
+                if(first==null) {
+                    first=prev;
+                    mid=root;
+                }
+                else
+                    last=root;
+            }
+            prev=root;
+            
+            swapcheck(root.right);
+        }
+    }
+}
