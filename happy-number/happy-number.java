24e82bbc622ca59b5d65fe3b08b8852166e67465
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-01-30 22:17:21 +0530

    Time: 1 ms (85.02%), Space: 38.3 MB (6.18%) - LeetHub

--- happy-number/happy-number.java
+++ happy-number/happy-number.java
@@ -1,28 +1,28 @@
 class Solution {
-    public boolean isHappy(int n) {
-        int a=n,d,rev;
-         
-        while(a>0)
-        {
-            int a1=a;
-            rev=0;
-            d=0;
-            if (a1 == 1 || a1 == 7) {  
-         return true;
-         } else if (a1 <= 9) {
-               return false;
-        }
-            while(a1>0){
-                d=a1%10;
-                rev=rev+(d*d);
-                a1=a1/10;
-            }
-            
-            if(rev==1)
-                return true;
-           
-                a=rev;
-        }
-        return false;
-    }
+    public boolean isHappy(int n) {
+        int a=n,d,rev;
+         
+        while(a>0)
+        {
+            int a1=a;
+            rev=0;
+            d=0;
+            if (a1 == 1 || a1 == 7) {  
+         return true;
+         } else if (a1 <= 9) {
+               return false;
+        }
+            while(a1>0){
+                d=a1%10;
+                rev=rev+(d*d);
+                a1=a1/10;
+            }
+            
+            if(rev==1)
+                return true;
+           
+                a=rev;
+        }
+        return false;
+    }
 }
