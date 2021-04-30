a6e766f07f257ea8a097f6d56bf63e12eb6bbf85
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-04-30 16:50:13 +0530

    Time: 0 ms (100.00%), Space: 37.4 MB (13.91%) - LeetHub

---
+++ spiral-matrix-ii/spiral-matrix-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int mat[][]=new int[n][n];
+        int i,k=0,l=0;
+        int num=1;
+        int m=n;
+        while(k<m && l<n){
+            //top row
+        for(i=l;i<n;i++)
+            mat[k][i]=num++;
+        k++;
+        
+        //last column
+        for(i=k;i<m;i++)
+            mat[i][n-1]=num++;
+        
+        n--;
+        
+        //bottom row
+        if(k<m){
+            for(i=n-1;i>=l;i--)
+                mat[m-1][i]=num++;
+            m--; 
+        }
+        //first column
+        if(l<n){
+            for(i=m-1;i>=k;i--)
+                mat[i][l]=num++;
+            l++; 
+        }
+        }
+        return mat;
+    }
+}
