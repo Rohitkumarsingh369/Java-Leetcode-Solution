c42edab44caa23bcb789ecd4e3b19699356ccaa8
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-05-02 16:15:09 +0530

    Time: 61 ms (49.04%), Space: 41.4 MB (40.33%) - LeetHub

--- sliding-window-median/sliding-window-median.java
+++ sliding-window-median/sliding-window-median.java
@@ -1,53 +1,63 @@
-public class Solution {
+class Solution {
+    private PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+    private PriorityQueue<Integer> maxHeap = new PriorityQueue<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer a, Integer b) {
+            return Integer.compare(b, a);
+        }
+    });
     public double[] medianSlidingWindow(int[] nums, int k) {
-        double[] res = new double[nums.length - k + 1];
-        int idx = 0;
-        boolean useBoth = k % 2 == 0;
-        TreeMap<Integer, Integer> small = new TreeMap<>((a, b)->{return (int)((double)b-a);});
-        int smallSize = 0; 
-        TreeMap<Integer, Integer> big = new TreeMap<>();
-        int bigSize = 0;
-        for(int i = 0; i<nums.length; i++){
-            if(smallSize + bigSize == k){
-                if(nums[i-k] <= small.firstKey()){
-                    remove(small, nums[i-k]);
-                    smallSize--;
-                }else{
-                    remove(big, nums[i-k]);
-                    bigSize--;
-                }
-            }
+        int n = nums.length - k + 1;
+        if (n <= 0) {
+            return new double[0];
+        }
+        double[] result = new double[n];
 
-            if(smallSize<=bigSize){
-                add(small, nums[i]);
-                smallSize++;
-            }else{
-                add(big, nums[i]);
-                bigSize++;
+        for (int i = 0; i <= nums.length; i++) {
+            if (i >= k) {
+                result[i - k] = getMedian();
+                remove(nums[i - k]);
             }
-            if(bigSize>0){
-                while(small.firstKey()>big.firstKey()){
-                    add(big, remove(small, small.firstKey()));
-                    add(small, remove(big, big.firstKey()));
-                }
-            }
-
-            if(smallSize + bigSize==k){
-                if(useBoth) res[idx++] = ((double)small.firstKey() + big.firstKey())/2.0;
-                else res[idx++] = (double)small.firstKey();
+            if (i < nums.length) {
+                add(nums[i]);
             }
         }
-        return res;
+        return result;
     }
 
-    private int remove(TreeMap<Integer, Integer> map, int i){
-        map.put(i, map.get(i)-1);
-        if(map.get(i)==0) map.remove(i);
-        return i; 
+    private void add(int num) {
+        if (num > getMedian()) {
+            minHeap.add(num);
+        } else {
+            maxHeap.add(num);
+        }
+        rebalance();
     }
 
-    private void add(TreeMap<Integer, Integer> map, int i){
-        if(!map.containsKey(i)) map.put(i, 1);
-        else map.put(i, map.get(i)+1);
+    private void remove(int num) {
+        if (num > getMedian()) {
+            minHeap.remove(num);
+        } else {
+            maxHeap.remove(num);
+        }
+        rebalance();
+    }
+
+    private double getMedian() {
+        if (minHeap.isEmpty() && maxHeap.isEmpty()) {
+            return 0;
+        } 
+        if (minHeap.size() == maxHeap.size()) {
+            return ((double) minHeap.peek() + (double) maxHeap.peek()) / 2.0;
+        }
+        return (double) maxHeap.peek();
+    }
+
+    private void rebalance() {
+        if (minHeap.size() > maxHeap.size()) {
+            maxHeap.add(minHeap.poll());
+        } else if (maxHeap.size() - minHeap.size() > 1) {
+            minHeap.add(maxHeap.poll());
+        }
     }
 }
