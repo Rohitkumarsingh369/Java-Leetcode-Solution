b6f0335593055373f0f149166dff38708aafc06e
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-05-07 13:52:05 +0530

    Time: 0 ms (100.00%), Space: 39 MB (97.51%) - LeetHub

--- delete-node-in-a-bst/delete-node-in-a-bst.java
+++ delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -15,43 +15,26 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-         if(root == null) return root;
-        if(key > root.val){ 
-            root.right = deleteNode(root.right, key);
-        }else if(key < root.val){ 
-            root.left = deleteNode(root.left, key);
-        }else{ 
-            if(root.left == null && root.right == null){
-                root = null;
-            }else if(root.right != null){ 
+        if(root==null) return root;
+        if(key>root.val)
+            root.right=deleteNode(root.right,key);
+        else if(key<root.val)
+            root.left=deleteNode(root.left,key);
+        else{
+            if(root.left==null && root.right==null)
+                root=null;
+            else if(root.right!=null){
                 root.val = successor(root); // my worthy successor
                 root.right = deleteNode(root.right, root.val);
-            }else{ 
+            }
+            else{
                 root.val = predecessor(root);
                 root.left = deleteNode(root.left, root.val);
             }
         }
         return root;
     }
-   /* public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null) return root;
-        if(key > root.val){ //move right
-            root.right = deleteNode(root.right, key);
-        }else if(key < root.val){ //move left
-            root.left = deleteNode(root.left, key);
-        }else{ //oh yes, we finally found the target
-            if(root.left == null && root.right == null){ //hmm, its a leaf node; easy peasy
-                root = null;
-            }else if(root.right != null){ // oh, it has a right child, don't make it an orphan or is it old enough to become a parent ? lets find out
-                root.val = successor(root); // my worthy successor
-                root.right = deleteNode(root.right, root.val);
-            }else{ //oh it seems that I do not have a worthy successor, fallback, fallback ...
-                root.val = predecessor(root);
-                root.left = deleteNode(root.left, root.val);
-            }
-        }
-        return root;
-    }*/
+  
     private int successor(TreeNode root){
         root = root.right;
         while(root.left != null){
