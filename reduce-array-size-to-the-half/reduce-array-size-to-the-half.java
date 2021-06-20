9429962a1b39a9536881e72106432cfb22658f8f
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-06-20 13:59:12 +0530

    Time: 36 ms (66.14%), Space: 49.3 MB (63.40%) - LeetHub

---
+++ reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -0,0 +1,25 @@
+class Solution {
+  
+        public int minSetSize(int[] arr) {
+        int len=arr.length;
+        int remaining=len;
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int a:arr){
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+      
+            //imp to note how to convert map to list
+    List<Integer>list=map.values().stream().collect(Collectors.toList());
+            
+            
+            //sort in reverse order
+    Collections.sort(list,Collections.reverseOrder());
+        int index=0;
+        while(remaining>len/2){
+            remaining-=list.get(index);
+            index++;
+        }
+        return index;
+    }
+   
+}
