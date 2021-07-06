d3a235f44880a75e056145375ef4446ca177af63
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-07-06 12:39:43 +0530

    Time: 36 ms (69.75%), Space: 48.8 MB (81.94%) - LeetHub

--- reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
+++ reduce-array-size-to-the-half/reduce-array-size-to-the-half.java
@@ -1,6 +1,5 @@
 class Solution {
-  
-        public int minSetSize(int[] arr) {
+    public int minSetSize(int[] arr) {
         int len=arr.length;
         int remaining=len;
         Map<Integer,Integer> map=new HashMap<>();
@@ -9,17 +8,17 @@ class Solution {
         }
       
             //imp to note how to convert map to list
-    List<Integer>list=map.values().stream().collect(Collectors.toList());
-            
-            
-            //sort in reverse order
-    Collections.sort(list,Collections.reverseOrder());
-        int index=0;
-        while(remaining>len/2){
-            remaining-=list.get(index);
-            index++;
-        }
-        return index;
+        List<Integer>list=map.values().stream().collect(Collectors.toList());
+
+
+                //sort in reverse order
+        Collections.sort(list,Collections.reverseOrder());
+            int index=0;
+            while(remaining>len/2){
+                remaining-=list.get(index);
+                index++;
+            }
+            return index;
     }
    
 }
