e987891b476918a48b2f13f568992c583afc0233
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-07-13 12:55:22 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (10.06%) - LeetHub

---
+++ find-peak-element/find-peak-element.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+         // First or last element is peak element
+        int n=arr.length;
+    if (n == 1)
+      return 0;
+    if (arr[0] >= arr[1])
+        return 0;
+    if (arr[n - 1] >= arr[n - 2])
+        return n - 1;
+ 
+    // Check for every other element
+    for(int i = 1; i < n - 1; i++)
+    {
+         
+        // Check if the neighbors are smaller
+        if (arr[i] >= arr[i - 1] &&
+            arr[i] >= arr[i + 1])
+            return i;
+    }
+    return 0;
+    }
+}
