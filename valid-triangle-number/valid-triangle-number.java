746c7687623aa50fbb62fb46cf1b6e4fec9880bf
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-07-16 11:49:13 +0530

    Time: 33 ms (69.35%), Space: 38.6 MB (56.99%) - LeetHub

---
+++ valid-triangle-number/valid-triangle-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int triangleNumber(int[] a) {
+        Arrays.sort(a);
+        int n=a.length;
+        int count=0;
+        for(int i=n-1;i>=1;i--){
+            int left=0,right=i-1;
+            while(left<right){
+                if(a[left]+a[right]>a[i]){
+                    count+=right-left;
+                    right--;
+                }
+                else
+                left++;
+            }
+        }
+        return count;
+    }
+}
