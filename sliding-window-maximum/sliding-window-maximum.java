546c9b7add69f4afc70d181ff1a676442b9c52c6
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-08-03 21:01:25 +0530

    Time: 29 ms (77.62%), Space: 54.2 MB (57.30%) - LeetHub

--- sliding-window-maximum/sliding-window-maximum.java
+++ sliding-window-maximum/sliding-window-maximum.java
@@ -1,6 +1,6 @@
 class Solution {
    
-         public int[] maxSlidingWindow(int[] a, int k) {
+     public int[] maxSlidingWindow(int[] a, int k) {
         int n=a.length;
         if(n<=0)return a;
 	        Deque<Integer> dq=new LinkedList<>();
@@ -25,5 +25,38 @@ class Solution {
 	        ans[i-k]=a[dq.peekFirst()];
 	        return ans;
     }
+   /* public int[] maxSlidingWindow(int[] a, int k) {
+        
+        //max heap
+        PriorityQueue<Integer> queue=new PriorityQueue<>(Collections.reverseOrder());
+        
+        //length of the array
+        int n=a.length;
+        
+        
+        
+        //return array
+        int result[]=new int[n-k+1];
+        int index=0;
+        
+        int i=0;
+        
+        for(;i<k;i++)
+            queue.add(a[i]);
+        
+        result[index++]=queue.peek();
+            queue.remove(a[0]);
+        
+        for(;i<n;i++){
+            
+            queue.add(a[i]);
+            
+            result[index++]=queue.peek();
+            
+            queue.remove(a[n-k+1]);
+        }
+        
+        return result;
+    }*/
    
 }
