92fad36089f3aa522d74ebb7692e1d5e16fa9b79
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2021-08-28 12:34:24 +0530

    Time: 3 ms (20.11%), Space: 38.9 MB (10.87%) - LeetHub

--- longest-uncommon-subsequence-ii/longest-uncommon-subsequence-ii.java
+++ longest-uncommon-subsequence-ii/longest-uncommon-subsequence-ii.java
@@ -1,9 +1,14 @@
 class Solution {
-    public int findLUSlength(String[] strs) {
+   /* public int findLUSlength(String[] strs) {
     Map<String, Integer> subseqFreq = new HashMap<>();
-    for (String s : strs) 
-        for (String subSeq : getSubseqs(s))
+    for (String s : strs) {
+       Set<String> result=getSubseqs(s);
+         System.out.println(result);
+        for (String subSeq : getSubseqs(s)){
+            
             subseqFreq.put(subSeq, subseqFreq.getOrDefault(subSeq, 0) + 1);
+        }
+    }
     int longest = -1;
     for (Map.Entry<String, Integer> entry : subseqFreq.entrySet()) 
         if (entry.getValue() == 1) longest = Math.max(longest, entry.getKey().length());
@@ -20,5 +25,43 @@ public static Set<String> getSubseqs(String s) {
     res.addAll(subRes);
     for (String seq : subRes) res.add(s.charAt(0) + seq);
     return res;
-}
+    
+}*/
+    public int findLUSlength(String[] strs) {
+        int len = strs.length;
+        
+        // reverse sorting array with length 
+        Arrays.sort(strs, new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return s2.length() - s1.length();
+            }
+        });
+        
+        for(int i=0; i<len; i++){
+            int missMatchCount = strs.length - 1;
+            for(int j=0; j<len; j++){
+                if(i != j && !isSubSequence(strs[i], strs[j])){
+                    missMatchCount --;
+                }
+            }
+            
+            // strs[i] is not a sub sequence of any other entry
+            if(missMatchCount == 0){
+                return strs[i].length();
+            }
+        }
+        
+        return -1;
+    }
+    
+    private boolean isSubSequence(String s1, String s2){
+        int idx = 0;
+        for(char ch : s2.toCharArray()){
+            if(idx < s1.length() && ch == s1.charAt(idx)){
+                idx++;
+            }
+        }
+        
+        return idx == s1.length();
+    }
 }
