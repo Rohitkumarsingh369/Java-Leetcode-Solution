fbea14c72d1a496e490fa7d2a3aeb63d7fd9659a
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-03-29 13:14:36 +0530

    Time: 28 ms (56.74%), Space: 79.3 MB (47.71%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
+   /* public int minDays(int[] bloomDay, int m, int k) {
         int n = bloomDay.length,min=bloomDay[0],max=bloomDay[0],res=-1;
         for(int i=1;i<n;i++){
             min=Math.min(min,bloomDay[i]);
@@ -32,5 +32,45 @@ class Solution {
             }
         }
         return bouquet>=m;
+    }*/
+    public int minDays(int[] bloomDay, int m, int k){
+        int n=bloomDay.length,min=bloomDay[0],max=bloomDay[0],result=-1;
+        
+        for(int i=0;i<n;i++){
+            min=Math.min(bloomDay[i],min);
+            max=Math.max(bloomDay[i],max);
+        }
+        
+        while(min<=max){
+            int mid=min+(max-min)/2;
+            
+            if(solve(bloomDay,mid,m,k)){
+                result=mid;
+                max=mid-1;
+            }
+            else{
+                min=mid+1;
+            }
+            
+        }
+        return result;
+    }
+    public boolean solve(int nums[],int day,int m,int k){
+     int flowercollected=0,bouqet=0;
+        
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=day){
+                flowercollected++;
+            }
+            else{
+                flowercollected=0;
+            }
+            if(k==flowercollected){
+                bouqet++;
+                flowercollected=0;
+            }
+        }
+        
+        return bouqet>=m;
     }
 }
