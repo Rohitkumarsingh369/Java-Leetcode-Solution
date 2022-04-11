a6cd6f0167a27ebde271edf12dda2ad580c1549e
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-04-12 01:28:57 +0530

    Time: 8 ms (65.89%), Space: 55.1 MB (18.99%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        List<List<Integer>> result = new ArrayList();
+        int rows = grid.length, col = grid[0].length;
+
+        for (int r = 0; r < rows; r++)// filling all the rows of result with empty lists
+            result.add(new ArrayList());
+        k %= (rows * col);// every rows*col shifts grid became the same, skipping the similar shifts
+        int dimension = rows * col;
+        int begin = dimension - k;// element at (begin) will be at 0,0 in new grid
+        int x = 0;// counter variable
+        for (int i = begin; i < begin + dimension; i++) {
+            int r = (i / col) % rows, c = i % col;// calculating row and cell for new grid
+            result.get(x / col).add(grid[r][c]);// adding new element in new row
+            x++;
+        }
+        return result;// returning result list
+    }
+}
