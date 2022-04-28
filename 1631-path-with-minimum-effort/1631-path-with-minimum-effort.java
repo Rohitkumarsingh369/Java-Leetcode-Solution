4283833bc269e19b0c7f1316adcbbaca9eed8e86
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-04-29 00:46:59 +0530

    Time: 79 ms (55.62%), Space: 42.8 MB (85.75%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minimumEffortPath(int[][] heights) {
+   /* public int minimumEffortPath(int[][] heights) {
         
         int n=heights.length;
         int m=heights[0].length;
@@ -39,5 +39,31 @@ class Solution {
             }
         }
         return efforts[n-1][m-1]; 
+    }*/
+    public int minimumEffortPath(int[][] heights) {
+        int left = 0, right = (int) 10e6;
+        while (left < right) {
+            int middle = (right + left) / 2;
+            if (dfs(heights, new boolean[heights.length][heights[0].length], 0, 0, heights[0][0], middle))
+                right = middle;
+            else
+                left = middle + 1;
+        }
+        return right;
+    }
+
+
+    private boolean dfs(int[][] heights, boolean[][] visited, int row, int column, int lastHeight, int threshold) {
+        if (row < 0 || row >= heights.length || column < 0 || column >= heights[0].length ||
+                visited[row][column] || Math.abs(heights[row][column] - lastHeight) > threshold)
+            return false;
+
+        visited[row][column] = true;
+
+        return ((row == heights.length - 1 && column == heights[0].length - 1)
+                || dfs(heights, visited, row + 1, column, heights[row][column], threshold)
+                || dfs(heights, visited, row - 1, column, heights[row][column], threshold)
+                || dfs(heights, visited, row, column + 1, heights[row][column], threshold)
+                || dfs(heights, visited, row, column - 1, heights[row][column], threshold));
     }
 }
