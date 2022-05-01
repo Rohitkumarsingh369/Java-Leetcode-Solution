99c974f3c06bdb3863c4c6735341dbb5edd76eea
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-01 13:29:25 +0530

    Time: 28 ms (20.43%), Space: 51 MB (12.11%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int countSubstrings(String s) {
+   /* public int countSubstrings(String s) {
         int n=s.length();
         int dp[][] = new int[n][n];
         int count=0;
@@ -31,5 +31,38 @@ class Solution {
  
         // return total palindromic substrings
         return count;
-    }
+    }*/
+    
+     public int countSubstrings(String s) {
+         int length=s.length();
+         int dp[][]=new int[length][length];
+         
+         int count=0;
+         
+         for(int size=0;size<length;size++){
+             for(int left=0,right=size;right<length;left++,right++){
+                 if(size==0){
+                     dp[left][right]=1;
+                 }
+                 else if(size==1){
+                     if(s.charAt(left)==s.charAt(right))
+                         dp[left][right]=1;
+                     else
+                         dp[left][right]=0;
+                 }
+                 else{
+                     if(s.charAt(left)==s.charAt(right) && dp[left+1][right-1]==1)
+                         dp[left][right]=1;
+                     else
+                         dp[left][right]=0;
+                 }
+                 
+                 
+                 if(dp[left][right]==1)
+                     count++;
+                     
+             }
+         }
+         return count;
+     }
 }
