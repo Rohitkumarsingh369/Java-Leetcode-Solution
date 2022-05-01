e16adc91d8ae4a1409a70a0e602c95eb59532698
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-01 13:45:02 +0530

    Time: 1 ms (93.27%), Space: 43.4 MB (60.29%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<String> generateParenthesis(int n) {
+   /* public List<String> generateParenthesis(int n) {
         
         List<String> result = new ArrayList<>();
       
@@ -25,6 +25,34 @@ class Solution {
             sb.append(")");
             backTracking(result, sb, open, close + 1, n);
             sb.deleteCharAt(sb.length() - 1);
+        }
+    }*/
+    
+    public List<String> generateParenthesis(int n){
+        List<String> result=new ArrayList<>();
+        
+        StringBuilder sb=new StringBuilder();
+        
+        backtracking(result,sb,0,0,n);
+        
+        return result;
+    }
+    
+    public void backtracking(List<String> result,StringBuilder sb,int open,int close,int length){
+        if(open==length && close==length){
+            result.add(sb.toString());
+        }
+        
+        if(open<length){
+            sb.append("(");
+            backtracking(result,sb,open+1,close,length);
+            sb.deleteCharAt(sb.length()-1);
+        }
+        
+        if(open>close){
+             sb.append(")");
+            backtracking(result,sb,open,close+1,length);
+             sb.deleteCharAt(sb.length()-1);
         }
     }
 }
