2e1a6608d80bf45548574eda0a4a299c0e06aa65
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-02 18:14:27 +0530

    Added solution - LeetHub

---
+++ Shortest Common Supersequence - GFG/shortest-common-supersequence.java
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int t=sc.nextInt();
+		
+		sc.nextLine();
+		while(t-->0)
+		{
+		   //taking String X and Y
+		   String S[]=sc.nextLine().split(" ");
+		   String X=S[0];
+		   String Y=S[1];
+		   
+		   //calling function shortestCommonSupersequence()
+		   System.out.println(new Solution().shortestCommonSupersequence(X, Y, X.length(), Y.length()));
+		}
+	}
+
+
+
+
+       
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find length of shortest common supersequence of two strings.
+    public static int shortestCommonSupersequence(String X,String Y,int m,int n)
+    {
+        //Your code here
+        int dp[][]=new int[m+1][n+1];
+        
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0)
+                    dp[i][j]=j;
+                else if(j==0)
+                    dp[i][j]=i;
+                else if(X.charAt(i-1)==Y.charAt(j-1))
+                    dp[i][j]=1+dp[i-1][j-1];
+                
+                else
+                    dp[i][j]=1+Math.min(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
