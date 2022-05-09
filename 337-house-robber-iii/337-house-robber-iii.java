74203eaea99966f9f468ba0e29f830710f3b1895
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-09 18:51:12 +0530

    Time: 5 ms (20.30%), Space: 44.9 MB (24.38%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.java
+++ 337-house-robber-iii/337-house-robber-iii.java
@@ -15,18 +15,27 @@
  */
 class Solution {
     public int rob(TreeNode root) {
-        int[] ans=helper(root);
-        return Math.max(ans[0],ans[1]);
+    Map<TreeNode, Integer> map = new HashMap<>();
+    return robSub(root, map);
+}
+
+private int robSub(TreeNode root, Map<TreeNode, Integer> map) {
+    if (root == null) return 0;
+    if (map.containsKey(root)) return map.get(root);
+    
+    int val = 0;
+    
+    if (root.left != null) {
+        val += robSub(root.left.left, map) + robSub(root.left.right, map);
     }
-    public int[] helper(TreeNode root){
-        if(root==null){
-            return new int[]{0,0};
-        }
-        int[] left=helper(root.left);
-        int[] right=helper(root.right);
-        
-        int withRobbery=root.val+left[1]+right[1];
-        int withoutRobbery=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
-        return new int[]{withRobbery,withoutRobbery};
+    
+    if (root.right != null) {
+        val += robSub(root.right.left, map) + robSub(root.right.right, map);
     }
+    
+    val = Math.max(val + root.val, robSub(root.left, map) + robSub(root.right, map));
+    map.put(root, val);
+    
+    return val;
+}
 }
