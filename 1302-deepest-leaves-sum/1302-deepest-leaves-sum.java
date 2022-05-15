413cafc59d1f7f4fdcbd215426f09526c2acb67d
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-15 22:14:46 +0530

    Time: 12 ms (19.19%), Space: 59.7 MB (21.18%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,7 +14,7 @@
  * }
  */
 class Solution {
-    public int deepestLeavesSum(TreeNode root) {
+   /* public int deepestLeavesSum(TreeNode root) {
         int max= maxdepth(root);
         return sum(root,max);
     }
@@ -30,5 +30,43 @@ class Solution {
             return root.val;
         
         return sum(root.left,max-1)+sum(root.right,max-1);
+    }*/
+    
+    public int deepestLeavesSum(TreeNode root) {
+        if (root == null) {
+            return root.val;
+        }
+
+        int sum = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelCount = queue.size();
+            boolean isDeepest = true;
+            sum = 0;
+            for (int i = 0; i < levelCount; i++) {
+                TreeNode curNode = queue.poll();
+				
+                if (curNode.left == null && curNode.right == null) {
+                    sum += curNode.val;
+                }
+				
+                if (curNode.left != null) {
+                    isDeepest = false;
+                    queue.offer(curNode.left);
+                }
+				
+                if (curNode.right != null) {
+                    isDeepest = false;
+                    queue.offer(curNode.right);
+                }
+            }
+
+            if (isDeepest && queue.isEmpty()) {
+                return sum;
+            }
+        }
+
+        return sum;
     }
 }
