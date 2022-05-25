d6bb8ff4e152a637fa1221af17f45a1b97270ba2
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-25 12:08:09 +0530

    Time: 91 ms (23.98%), Space: 79.3 MB (80.39%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -0,0 +1,43 @@
+class Solution {
+    // envelopes = [[w, h], [w, h]...]
+    public int maxEnvelopes(int[][] envelopes) {
+        int n = envelopes.length;
+        // sort by ascending width, and sort by descending height if the width are the same
+        Arrays.sort(envelopes, new Comparator<int[]>() 
+        {
+            public int compare(int[] a, int[] b) {
+                return a[0] == b[0] ? 
+                    b[1] - a[1] : a[0] - b[0];
+            }
+        });
+        // find LIS on the height array
+        int[] height = new int[n];
+        for (int i = 0; i < n; i++)
+            height[i] = envelopes[i][1];
+
+        return lengthOfLIS(height);
+    }
+        /* returns the length of LIS in nums */
+    public int lengthOfLIS(int[] nums) {
+        int piles = 0, n = nums.length;
+        int[] top = new int[n];
+        for (int i = 0; i < n; i++) {
+            // playing card to process
+            int poker = nums[i];
+            int left = 0, right = piles;
+            // position to insert for binary search
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (top[mid] >= poker)
+                    right = mid;
+                else
+                    left = mid + 1;
+            }
+            if (left == piles) piles++;
+            // put this playing cart on top of the pile
+            top[left] = poker;
+        }
+        // the number of cards is the LIS length
+        return piles;
+    }
+}
