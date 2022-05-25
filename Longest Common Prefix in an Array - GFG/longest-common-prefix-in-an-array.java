9b87ed12339888c4e43016b249f24bca6f2274d1
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-26 01:38:38 +0530

    Added solution - LeetHub

--- Longest Common Prefix in an Array - GFG/longest-common-prefix-in-an-array.java
+++ Longest Common Prefix in an Array - GFG/longest-common-prefix-in-an-array.java
@@ -26,7 +26,7 @@ class GFG
 
 class Solution{
     String longestCommonPrefix(String arr[], int n){
-       String small = arr[0];
+       /*String small = arr[0];
        //find smallest string length
        for(int i=1; i<n; i++)
        {
@@ -51,6 +51,28 @@ class Solution{
                }
            }
        }
-       return small;
+       return small;*/
+       
+        // code here
+        if(n==0 || arr==null)
+        return "-1";
+        
+        String lcp=arr[0]; //Assuming that the first word is the ans (i.e- geeksforgeeks)
+        for(int i=1;i<n;i++) //starts checking from the 2nd word in the array that's why i=1
+        {
+            //will check the length and compare the characters
+            String currword=arr[i]; //Stores the current words(for first it's - geeks)
+            int j=0;
+            while(j<lcp.length() && j<currword.length() && lcp.charAt(j)==currword.charAt(j))
+            {
+                j++;
+            }
+            if(j==0) //while loop terminated at first execution only 
+            {   //i.e. there's no common prefix may be the words are {hello, world}
+                return "-1";
+            }
+            lcp=currword.substring(0,j);
+        }
+        return lcp;
    }
 }
