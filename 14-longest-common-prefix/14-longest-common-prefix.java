28b4b8d2bd831bdb52b525694ab7afabc8959f6f
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-26 01:39:28 +0530

    Time: 2 ms (45.30%), Space: 42.3 MB (43.00%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -1,15 +1,26 @@
 class Solution {
-    public String longestCommonPrefix(String[] str) {
-         if(str.length == 0) return "";
+    public String longestCommonPrefix(String[] arr) {
+          // code here
+        int n=arr.length;
+        if(n==0 || arr==null)
+        return "";
         
-        String s = str[0];
-        
-        for(int i = 1; i<str.length; i++)
-            while(str[i].indexOf(s) != 0){
-                s = s.substring(0,s.length()-1);
+        String lcp=arr[0]; //Assuming that the first word is the ans (i.e- geeksforgeeks)
+        for(int i=1;i<n;i++) //starts checking from the 2nd word in the array that's why i=1
+        {
+            //will check the length and compare the characters
+            String currword=arr[i]; //Stores the current words(for first it's - geeks)
+            int j=0;
+            while(j<lcp.length() && j<currword.length() && lcp.charAt(j)==currword.charAt(j))
+            {
+                j++;
             }
-       
-        
-        return s;
+            if(j==0) //while loop terminated at first execution only 
+            {   //i.e. there's no common prefix may be the words are {hello, world}
+                return "";
+            }
+            lcp=currword.substring(0,j);
+        }
+        return lcp;
     }
 }
