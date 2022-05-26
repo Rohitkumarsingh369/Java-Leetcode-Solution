aa78ba30c62bd8bb3f31cbed634edbaad54e7c5f
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-26 13:13:52 +0530

    Time: 349 ms (8.81%), Space: 134.6 MB (16.82%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,61 +1,35 @@
 class Solution {
-    static int count;
-    public int minReorder(int n, int[][] con) {
-        count=0;
-        ArrayList<ArrayList<Integer>> graph=new ArrayList<>();
-         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++)
-        {
-            graph.add(new ArrayList<>());
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<con.length;i++)
-        {
-            int u=con[i][0];
-            int v=con[i][1];
-            graph.get(u).add(v);
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+    int total =0;
+    public int minReorder(int n, int[][] connections) {
+        //note can not add new Edge
+        Map<Integer, List<Integer>>  map = new HashMap<>();
+        Set<String> direction = new HashSet<>();
+        
+        for(int[] connection : connections){
+            int a = connection[0], b = connection[1];
+            direction.add(a+"-"+b);
+
+            if(!map.containsKey(b)) map.put(b, new ArrayList());
+            if(!map.containsKey(a)) map.put(a, new ArrayList());
+            map.get(a).add(b);
+            map.get(b).add(a);
         }
         
-        boolean[] visited=new boolean[n];
-        dfs(graph,0,visited,adj);
-            
-        
-        
-        return count;
-    }
-    static boolean containsEdge(int src , int dec ,  ArrayList<ArrayList<Integer>> graph){
-        
-        for(Integer nn : graph.get(src)){
-            if(nn == dec) 
-                return false;
-        }
-        return true;
+        dfs(0, map, direction, new int[n]);
+        return n-1-total;
     }
     
-    public static void dfs(ArrayList<ArrayList<Integer>> graph,int i,boolean[] visited,ArrayList<ArrayList<Integer>> adj)
-    {
-        if(visited[i]==true)
-        {
-            return;
-        }
+    public void dfs(int source, Map<Integer, List<Integer>> map, Set<String> direction, int[] visited){
+        if(visited[source]==1) return;
+        visited[source] = 1;
         
-        
-        visited[i]=true;
-        
-        for(int u: adj.get(i))
-        {
-            if(visited[u]==false)
-            {
-                if(!containsEdge(i,u,graph))
-                 count++;
-                
-                
-                dfs(graph,u,visited,adj);    
-                
-               
+        for(int child : map.get(source)){
+            if(visited[child]==1) continue;
+            if(!direction.contains(source+"-"+child)){
+                total++;
             }
+            dfs(child, map, direction, visited);
         }
     }
+
 }
