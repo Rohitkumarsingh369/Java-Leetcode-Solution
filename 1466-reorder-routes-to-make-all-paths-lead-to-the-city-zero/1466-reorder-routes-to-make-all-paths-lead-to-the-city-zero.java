e3fd30e4f07146362fa106284f943b3efeac682e
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-26 13:16:59 +0530

    Time: 56 ms (87.84%), Space: 105.1 MB (64.03%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -1,5 +1,5 @@
-class Solution {
-    int total =0;
+/*class Solution {
+int total =0;
     public int minReorder(int n, int[][] connections) {
         //note can not add new Edge
         Map<Integer, List<Integer>>  map = new HashMap<>();
@@ -32,4 +32,33 @@ class Solution {
         }
     }
 
+}*/
+class Solution {
+  public int minReorder(int n, int[][] connections) {
+    List<Integer>[] graph = new List[n];
+
+    for (int i = 0; i < n; ++i)
+      graph[i] = new ArrayList<>();
+
+    for (int[] conn : connections) {
+      graph[conn[0]].add(conn[1]);
+      graph[conn[1]].add(-conn[0]); // - := conn[0] -> conn[1]
+    }
+
+    return dfs(graph, 0, -1);
+  }
+
+  private int dfs(List<Integer>[] graph, int u, int parent) {
+    int change = 0;
+
+    for (final int v : graph[u]) {
+      if (Math.abs(v) == parent)
+        continue;
+      if (v > 0)
+        ++change;
+      change += dfs(graph, Math.abs(v), u);
+    }
+
+    return change;
+  }
 }
