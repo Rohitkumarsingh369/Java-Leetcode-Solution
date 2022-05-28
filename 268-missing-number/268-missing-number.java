15786a8372ca19a01cd7ee0a9ca1daa95d063140
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-05-28 11:25:34 +0530

    Time: 1 ms (65.97%), Space: 50.9 MB (58.77%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int missingNumber(int[] nums) {
          int n = nums.length;
         // Sum = (no. of elements + 1)(1st element + last element)/2
-        int expectedSum = (n+1)*(0+n)/2;
+        int expectedSum = (n+1)*(n)/2;
         
         int actualSum = 0;
         for(int i : nums)
