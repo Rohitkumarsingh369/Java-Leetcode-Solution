ee5af9ac64e933acb9f0d86e7a167d3ff97e631d
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-06-14 11:01:19 +0530

    Time: 3 ms (54.01%), Space: 80.1 MB (60.54%) - LeetHub

--- 134-gas-station/134-gas-station.java
+++ 134-gas-station/134-gas-station.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
+    /*public int canCompleteCircuit(int[] gas, int[] cost) {
         int gasremaining=0;
         int total=0;
         int start=0;
@@ -18,6 +18,39 @@ class Solution {
         }
         
         return total>=0?start:-1;
+        
+    }*/
+    /* int n = gas.length;
+        int total_surplus = 0;
+        int surplus = 0;
+        int start = 0;
+        
+        for(int i = 0; i < n; i++){
+            total_surplus += gas[i] - cost[i];
+            surplus += gas[i] - cost[i];
+            if(surplus < 0){
+                surplus = 0;
+                start = i + 1;
+            }
+        }
+        return (total_surplus < 0) ? -1 : start;*/
+     public int canCompleteCircuit(int[] gas, int[] cost) {
+        int remaining=0;
+        int total=0;
+        int start=0;
+        
+        for(int i=0;i<gas.length;i++){
+            remaining+=gas[i]-cost[i];
+            total+=gas[i]-cost[i];
+            if(remaining<0){
+            
+                remaining=0;
+                start=i+1;
+            }
+           
+        }
+        
+        return total<0?-1:start;
         
     }
 }
