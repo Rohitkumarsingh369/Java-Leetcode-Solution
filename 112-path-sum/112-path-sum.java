282ea08c04f7cb58c2c94f7665ccf498b297ad06
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-06-16 23:26:20 +0530

    Time: 6 ms (5.37%), Space: 44.3 MB (12.84%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+/*class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
        if(root==null)
            return false;
@@ -24,4 +24,33 @@ class Solution {
         return hasPathSum(root.left,targetSum-root.val)||hasPathSum(root.right,targetSum-root.val);
     }
     
+}*/
+class Solution {
+public boolean hasPathSum(TreeNode root, int targetSum) {
+if (root==null) return false;
+Stack<TreeNode> stack = new Stack<>();
+Stack<Integer> sum_stack = new Stack<>();
+stack.push(root);
+sum_stack.push(targetSum-root.val);
+
+    while(!stack.isEmpty()){
+        TreeNode temp_node = stack.pop();
+        int current_sum = sum_stack.pop();
+        
+        if (current_sum==0 && temp_node.left==null && temp_node.right==null){
+            return true;
+        }
+        
+        if (temp_node.right!=null){
+            stack.push(temp_node.right);
+            sum_stack.push(current_sum-temp_node.right.val);
+        }
+        if (temp_node.left!=null){
+            stack.push(temp_node.left);
+            sum_stack.push(current_sum-temp_node.left.val);
+        }
+    }
+    
+    return false;
+}
 }
