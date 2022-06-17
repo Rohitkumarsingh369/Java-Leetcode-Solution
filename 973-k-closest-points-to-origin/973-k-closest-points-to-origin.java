959a9e0cccbc529f01610e84e706e4739f3d1471
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-06-18 01:59:03 +0530

    Time: 18 ms (91.87%), Space: 114.3 MB (68.61%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,16 +1,28 @@
 class Solution {
-    public int[][] kClosest(int[][] points, int K) {
-    PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
-    for (int[] p : points) {
-        pq.offer(p);
-        if (pq.size() > K) {
-            pq.poll();
+    public int[][] kClosest(int[][] points, int k) {
+        int length=points.length;
+        int result[][]=new int[k][2];
+        
+        int distance[]=new int[length];
+        for(int i=0;i<length;i++){
+            int x=points[i][0];
+            int y=points[i][1];
+            distance[i]=(x*x)+(y*y);
         }
+        Arrays.sort(distance);
+        int distancek=distance[k-1];
+        
+        int index=0;
+        for(int i=0;i<length;i++){
+            int x=points[i][0];
+            int y=points[i][1];
+            int dist=(x*x)+(y*y);
+            if(dist<=distancek){
+                result[index][0]=x;
+                result[index][1]=y;
+                index++;
+            }
+        }
+        return result;
     }
-    int[][] res = new int[K][2];
-    while (K > 0) {
-        res[--K] = pq.poll();
-    }
-    return res;
-}
 }
