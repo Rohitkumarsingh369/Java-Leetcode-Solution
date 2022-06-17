9fb01482d933fb86128bb52aacc042d5557a6b2f
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-06-18 01:53:41 +0530

    Time: 1 ms (99.83%), Space: 43.7 MB (82.02%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] kWeakestRows(int[][] mat, int k) {
+   /* public int[] kWeakestRows(int[][] mat, int k) {
         int[] soldiers = new int[mat.length];
         ArrayList<Integer> sorted = new ArrayList<>();
 		//Counting soldiers
@@ -37,6 +37,38 @@ class Solution {
             result[i] = sorted.get(i);
         }
         return result;
+    }*/
+    
+
+    public int[] kWeakestRows(int[][] mat, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] != b[0] ? b[0] - a[0] : b[1] - a[1]);
+        int[] ans = new int[k];
+        
+        for (int i = 0; i < mat.length; i++) {
+            pq.offer(new int[] {numOnes(mat[i]), i});
+            if (pq.size() > k)
+                pq.poll();
+        }
+        
+        while (k > 0)
+            ans[--k] = pq.poll()[1];
+        
+        return ans;
     }
     
+    private int numOnes(int[] row) {
+        int lo = 0;
+        int hi = row.length;
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (row[mid] == 1)
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        
+        return lo;
+    }
 }
