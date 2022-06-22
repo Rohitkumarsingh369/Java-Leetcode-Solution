f70ec9ff04a5fd573783b4f2e4283cd39464f3e7
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-06-22 12:30:32 +0530

    Added solution - LeetHub

---
+++ 3. Max and Second Max - GFG/3.-max-and-second-max.java
@@ -0,0 +1,71 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+ // } Driver Code Ends
+        
+
+
+// Solution class to implement function largestAndSecondLargest
+class Solution{
+    
+    // Function to find largest and second largest element in the array
+    public static ArrayList<Integer> largestAndSecondLargest(int sizeOfArray, int arr[])
+    {
+        //code here.
+        ArrayList<Integer> result=new ArrayList<>();
+        int first_max=-1,second_max=-1;
+        
+        for(int i=0;i<sizeOfArray;i++){
+            if(arr[i]>first_max){
+                second_max=first_max;
+                first_max=arr[i];
+            }
+            else if(arr[i]<first_max && arr[i]>second_max){
+                second_max=arr[i];
+            }
+        }
+        
+        result.add(first_max);
+        result.add(second_max);
+        return result;
+    }
+}
+
+// { Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    int sizeOfArray = Integer.parseInt(br.readLine());
+		    
+		    int arr[] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int index = 0;index < sizeOfArray; index++){
+		        arr[index] = Integer.parseInt(elements[index]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		    res = obj.largestAndSecondLargest(sizeOfArray, arr);
+		    System.out.println(res.get(0) + " " + res.get(1) );
+		}
+	}
+}  // } Driver Code Ends
