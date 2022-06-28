58be239a09960c9d99433a7b285ba19830b22d7b
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-06-28 10:04:58 +0530

    Added solution - LeetHub

--- 1. Finding middle element in a linked list - GFG/1.-finding-middle-element-in-a-linked-list.java
+++ 1. Finding middle element in a linked list - GFG/1.-finding-middle-element-in-a-linked-list.java
@@ -57,15 +57,13 @@ class Solution
 {
     int getMiddle(Node head)
     {
-         // Your code here.
-         Node slow=head;
-         Node fast=head;
-         while(fast!=null && fast.next!=null){
-             slow=slow.next;
-             fast=fast.next.next;
-             
-         }
-         
-         return slow.data;
+        
+        Node slow=head,fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        
+        return slow.data;
     }
 }
