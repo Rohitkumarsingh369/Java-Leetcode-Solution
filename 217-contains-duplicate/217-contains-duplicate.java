d612b37149013f434cdc8c98c996f8e69476201c
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-06-28 11:48:35 +0530

    Time: 11 ms (65.29%), Space: 70.9 MB (28.70%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> distinct = new HashSet<Integer>();
+        for(int num : nums) {
+            if(distinct.contains(num)) {
+                return true;
+            }
+            distinct.add(num);
+        }
+        return false;
+    }
+}
