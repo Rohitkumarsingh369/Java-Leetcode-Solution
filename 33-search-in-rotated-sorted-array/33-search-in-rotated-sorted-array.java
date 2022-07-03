5b26a4c1c6ec1a6b5ccb8c74afea351f2e5e1b07
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-03 19:55:46 +0530

    Time: 1 ms (72.61%), Space: 42.6 MB (46.92%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,29 +1,27 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if(nums.length == 0){
-            return -1;
-        }
-        
-        return helper(nums,target,0,nums.length-1);  
+        int result=bsearch(nums,0,nums.length-1,target);
+        return result;
     }
-    
-    public int helper(int[] nums, int tar, int low, int high){
-        if(low > high){
+    static int bsearch(int nums[],int low,int high,int target){
+        if(low>high)
             return -1;
-        }
-        int mid = low+(high-low)/2;
+        int mid=low+(high-low)/2;
         
-        if(nums[mid] == tar){
+        if(nums[mid]==target)
             return mid;
+        
+        if(nums[low]<=nums[mid]){
+            
+            if(target>=nums[low]&&target<=nums[mid])
+                return bsearch(nums,low,mid-1,target);
+            
+          return bsearch(nums,mid+1,high,target);  
         }
+        if(target>=nums[mid]&&target<=nums[high])
+                return bsearch(nums,mid+1,high,target);
+            
+          return bsearch(nums,low,mid-1,target);  
         
-        int left = helper(nums,tar,low,mid-1);
-        int right = helper(nums, tar, mid+1, high);
-        
-        if(left != -1 || right != -1){
-            return left == -1 ? right : left;
-        }
-        
-        return -1;
     }
 }
