012cab244e7f45d5a6a2cf4359b59fa8acd4ab91
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-03 20:27:42 +0530

    Time: 1 ms (72.61%), Space: 42.4 MB (67.49%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int search(int[] nums, int target) {
+    /*public int search(int[] nums, int target) {
         if(nums.length==0)
             return -1;
         
@@ -22,6 +22,38 @@ class Solution {
         
         if(left!=-1 || right!=-1){
             return left!=-1?left:right;
+        }
+        
+        return -1;
+    }*/
+    
+    public int search(int[] nums, int target) {
+        if(nums==null || nums.length==0) return -1;
+        int l=0, r=nums.length-1, m=0;
+       // find out the index of the smallest element.
+        while(l<r){
+            m = (l+r)/2;
+            if(nums[m] > nums[r]){
+                l = m+1;
+            }else{
+                r = m;
+            }
+        }
+        
+       // since we now know the start, find out if the target is to left or right of start in the array.
+        int s = l;
+        l = 0; r = nums.length-1;
+        if(target >= nums[s] && target <= nums[r]){
+            l = s;
+        }else{
+            r = s;
+        }
+        // the regular search.
+        while(l<=r){
+            m = (l+r)/2;
+            if(nums[m]==target) return m;
+            else if(nums[m] > target) r = m-1;
+            else l=m+1;
         }
         
         return -1;
