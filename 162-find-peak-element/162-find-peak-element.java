f2ec037600e80bca093414855bd0941764a75e89
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-03 14:21:21 +0530

    Time: 1 ms (12.26%), Space: 42.8 MB (59.98%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.java
+++ 162-find-peak-element/162-find-peak-element.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int findPeakElement(int[] arr) {
+    /*public int findPeakElement(int[] num) {
      // First or last element is peak element
             int n=arr.length;
         if (n == 1)
@@ -19,5 +19,21 @@ class Solution {
                 return i;
         }
         return 0;
+    }*/
+    
+    public int findPeakElement(int[] num) {
+    int low = 0;
+        int high = num.length-1;
+        
+        while(low < high)
+        {
+            int mid1 = (low+high)/2;
+            int mid2 = mid1+1;
+            if(num[mid1] < num[mid2])
+                low = mid2;
+            else
+                high = mid1;
+        }
+        return low;
     }
 }
