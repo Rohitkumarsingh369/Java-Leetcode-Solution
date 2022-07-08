d63e2ac2ca1c255ca32c4ccf8e2ade46a1bb4052
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-08 12:49:16 +0530

    Time: 2 ms (91.96%), Space: 42.6 MB (94.92%) - LeetHub

---
+++ 55-jump-game/55-jump-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxLocation = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(maxLocation<i) return false; // if previous maxLocation smaller than i, meaning we cannot reach location i, thus return false.
+            maxLocation = (i+nums[i]) > maxLocation ? i+nums[i] : maxLocation; // greedy:
+        }
+        return true;
+    }
+}
