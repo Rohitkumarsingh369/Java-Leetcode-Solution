973a7fd348e8456a94f5e50f5e2b76847340c947
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-08 23:17:57 +0530

    Time: 2 ms (68.10%), Space: 42.4 MB (54.02%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numDecodings(String str) {
+        int n=str.length();
+       
+        int count[]=new int[n+1];
+        
+        count[0]=1;
+        count[1]=1;
+        
+        if(str.charAt(0)=='0')
+        return 0;
+        for(int i=2;i<=n;i++){
+            count[i]=0;
+            if(str.charAt(i-1)>'0')
+                count[i]=count[i-1];
+            
+            if(str.charAt(i-2)=='1'||(str.charAt(i-2)=='2'&& str.charAt(i-1)<'7'))
+                count[i]+=count[i-2];
+        }
+        
+        return count[n];
+    }
+}
