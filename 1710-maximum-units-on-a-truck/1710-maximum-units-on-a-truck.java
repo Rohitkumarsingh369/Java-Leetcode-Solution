fe828f502a06353f25ca9f595a5c7f53a0fe8214
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-08 12:37:35 +0530

    Time: 16 ms (31.89%), Space: 51.3 MB (21.14%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.java
@@ -2,11 +2,24 @@ class Solution {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
         
         int n=boxTypes.length;
-        Arrays.sort(boxTypes,(a, b) -> b[1] - a[1]);
-        
+       // Arrays.sort(boxTypes,(a, b) -> b[1] - a[1]); may cause int overflow
+        Arrays.sort(boxTypes,(a,b)->Integer.compare(b[1],a[1]));
         int i=0;
         
         int result=0;
+        
+        while(truckSize>0 && i<n){
+            int size=Math.min(truckSize,boxTypes[i][0]);
+            
+            truckSize-=size;
+            
+            result+=size*boxTypes[i][1];
+            
+            i++;
+        }
+        
+        return result;
+        /*int result=0;
         while(truckSize>0 && i<n){
             int size=Math.min(boxTypes[i][0],truckSize);
             truckSize-=size;
@@ -14,6 +27,21 @@ class Solution {
             result+=size*boxTypes[i][1];
             i++;
         }
-        return result;
+        return result;*/
     }
 }
+
+
+/*// Arrays.sort(boxTypes, (a, b) -> b[1] - a[1]); // b[1] - a[1] may cause int overflow, credit to @Zudas.
+        Arrays.sort(boxTypes, (a, b) -> Integer.compare(b[1], a[1]));
+        int boxes = 0;
+        for (int[] box : boxTypes) {
+            if (truckSize >= box[0]) {
+                boxes += box[0] * box[1];
+                truckSize -= box[0];
+            }else {
+                boxes += truckSize * box[1];
+                return boxes;
+            }
+        }
+        return boxes;*/
