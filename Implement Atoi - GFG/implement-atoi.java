dc2940102f124a79ca3f87c67c62a58ff6e57074
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-10 01:51:37 +0530

    Added solution - LeetHub

---
+++ Implement Atoi - GFG/implement-atoi.java
@@ -0,0 +1,59 @@
+// { Driver Code Starts
+//Initial template for JAVA
+
+import java.util.Scanner;
+
+class aToi
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String str = sc.nextLine();
+			
+			Solution obj = new Solution();
+			System.out.println(obj.atoi(str));
+		t--;
+		}
+	}
+}// } Driver Code Ends
+
+
+//User function template for JAVA
+
+/*You are required to complete this method */
+class Solution
+{
+    int atoi(String s) {
+	String ans = "0";
+        char[]  arr = s.toCharArray();
+        int ind = 0,sign = 0;
+        
+        //1) ignore the space
+        while(ind<arr.length && arr[ind]==' ') ind++;
+        
+          //2) get positive or negative
+        if(ind<arr.length && (arr[ind]=='+'||arr[ind]=='-')){
+            sign = arr[ind++]=='+'?0:1;
+        }
+        
+        for(;ind<arr.length;ind++){
+            //3) non digit
+            if(!Character.isDigit(arr[ind])) 
+               return -1;
+            // add the integer
+            ans+=arr[ind];
+        }
+        return check(ans,sign);
+    }
+    static int check(String s,int sign){
+        try{
+            return sign==1?-Integer.parseInt(s):Integer.parseInt(s);
+        }catch(Exception e){
+            return sign==1?Integer.MIN_VALUE:Integer.MAX_VALUE;
+        }
+    }
+}
