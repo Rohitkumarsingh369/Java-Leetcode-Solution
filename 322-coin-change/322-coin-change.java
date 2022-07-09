38caf65ecc56148b7263807fa70a4f4770e5789a
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-09 19:39:41 +0530

    Time: 17 ms (91.77%), Space: 46.1 MB (24.75%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,28 +1,21 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int table[]=new int[amount+1];
-        table[0]=0;
-        
-        for(int i=1;i<table.length;i++)
-        {
-            table[i]=Integer.MAX_VALUE;
-        }
-        
-        for(int i=1;i<=amount;i++)
-        {
-            for(int j=0;j<coins.length;j++)
-            {
-                if(coins[j]<=i){
-                int subres=table[i-coins[j]];
-                if(subres!=Integer.MAX_VALUE &&subres+1<table[i])
-                    table[i]=subres+1;
-                }
-            }
-        }
-            if(table[amount]==Integer.MAX_VALUE)
-                return -1;
-            
-            return table[amount];
+       int dp[] = new int[amount + 1];
+
+  Arrays.fill(dp, amount + 1);
+  dp[0] = 0;
+    
+  for(int i = 1; i <= amount; i++){
+      for(int coin_denomination : coins){
+     //if coin value/denomination is less than or equal to amount     
+          if(coin_denomination <= i){
+          dp[i] = Math.min(dp[i], dp[i - coin_denomination] + 1);
+          }
+      }
+  }
+ 
+    //overflow conditon
+    return dp[amount] > amount ? -1 : dp[amount];
         
     }
 }
