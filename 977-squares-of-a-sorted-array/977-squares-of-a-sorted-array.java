30a49cba272f1810493fae1e85fc24093e348ca6
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-12 01:32:36 +0530

    Time: 1 ms (100.00%), Space: 44.3 MB (85.69%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.java
@@ -1,49 +1,40 @@
 class Solution {
-    public int[] sortedSquares(int[] nums) {
-        int[] ans = new int[nums.length];
-        if (nums.length == 1){
-            nums[0] = nums[0]*nums[0];
-            return nums; 
-        }
-        else{
-            int i = search(nums); // non -ve pointer
-            int j = i - 1;//-ve pointer
-            int count = 0;
-            while (i < nums.length && j >= 0){
-                if (nums[i] <= -1 * nums[j]){
-                    ans[count] = nums[i]*nums[i];
-                    count++;
-                    i++;
-                }else{
-                    ans[count] = nums[j]*nums[j];
-                    count++;
-                    j--;
-                }
-            }
-            while (i < nums.length){//square rest of the non negative values
-                ans[count] = nums[i]*nums[i];
-                count++;
+    public int[] sortedSquares(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        int idx = 0;
+        int i = getIdxOfFirstPostive(arr);
+        int j = i - 1;
+        while (i < n && j >= 0) {
+            if (-1 * arr[j] > arr[i]) {
+                ans[idx++] = arr[i] * arr[i];
                 i++;
-            }
-            while (j >= 0){//square rest of the negative integers
-                ans[count] = nums[j]*nums[j];
-                count++;
+            } else {
+                ans[idx++] = arr[j] * arr[j];
                 j--;
             }
         }
+        while (i < n) {
+            ans[idx++] = arr[i] * arr[i];
+            i++;
+        }
+        while (j >= 0) {
+            ans[idx++] = arr[j] * arr[j];
+            j--;
+        }
         return ans;
     }
-    int search(int[] nums){//bs to find least non negative index
-        int start = 0 , end = nums.length - 1;
-        while (start < end){
-            int mid = start + (end - start)/2 ;
-            if (nums[mid] < 0){
+    int getIdxOfFirstPostive(int[] arr) {
+        int start = 0, end = arr.length - 1, ans = arr.length;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] >= 0) {
+                ans = mid;
+                end = mid - 1;
+            } else {
                 start = mid + 1;
             }
-            else{
-                end = mid;
-            }
         }
-        return start;
+        return ans;
     }
 }
