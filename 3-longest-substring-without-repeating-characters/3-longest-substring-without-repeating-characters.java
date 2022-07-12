8d8911042b2f1e37a2a6fe17524fac0a3f005a91
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-12 14:40:26 +0530

    Time: 6 ms (85.35%), Space: 44.3 MB (70.35%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -3,12 +3,21 @@ class Solution {
         int n=s.length();
         int lastindex[]=new int[256];
         Arrays.fill(lastindex,-1);
+        int start=0,end=0;
         int i=0,res=0;
         for(int j=0;j<n;j++){
             i=Math.max(i,lastindex[s.charAt(j)]+1);
-            res=Math.max(res,j-i+1);
+            
+            if(res<j-i+1){
+                res=j-i+1;
+               /* start=i;
+                end=j;*/
+                
+            }
+           
             lastindex[s.charAt(j)]=j;
         }
+       // System.out.println(s.substring(start,(end+1)));
         return res;
     }
 }
