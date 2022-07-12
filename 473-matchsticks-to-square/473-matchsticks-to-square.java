5348f1a1fae1252b1ea4c47ed60fbe8d2fe57f56
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-12 06:25:11 +0530

    Time: 290 ms (20.47%), Space: 41.8 MB (63.99%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.java
+++ 473-matchsticks-to-square/473-matchsticks-to-square.java
@@ -1,26 +1,41 @@
 class Solution {
-    public boolean makesquare(int[] nums) {
-        Long sum=0l;
-        for(int x:nums){
-            sum=sum+x;
-        }
-        if(sum%4!=0||nums.length<4) return false;
-        long width=(sum/4);
-        Arrays.sort(nums);
-        long sum1=0,sum2=0,sum3=0,sum4=0;
-        return helper(nums,nums.length-1,sum1,sum2,sum3,sum4,width);
+   public boolean makesquare(int[] nums) {
+     if (nums == null || nums.length < 4) return false;
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 4 != 0) return false;
         
+        Arrays.sort(nums);
+        reverse(nums);
+        
+     return dfs(nums, new int[4], 0, sum / 4);
     }
-    public boolean helper(int[] a, int i,long sum1,long sum2,long sum3,long sum4, long width){
-        if(sum1>width||sum2>width||sum3>width||sum4>width) return false;
-        if(i==-1){
-            if(sum1==width&&sum2==width&&sum3==width&&sum4==width) return true;
-            else return false;
+    
+    private boolean dfs(int[] nums, int[] sums, int index, int target) {
+     if (index == nums.length) {
+         if (sums[0] == target && sums[1] == target && sums[2] == target) {
+      return true;
+         }
+         return false;
+     }
+     
+     for (int i = 0; i < 4; i++) {
+         if (sums[i] + nums[index] > target) continue;
+         sums[i] += nums[index];
+            if (dfs(nums, sums, index + 1, target)) return true;
+         sums[i] -= nums[index];
+     }
+     
+     return false;
+    }
+    
+    private void reverse(int[] nums) {
+        int i = 0, j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++; j--;
         }
-//check a[i]  belonging to side1,side2,side3,side4
-        return helper(a,i-1,sum1+a[i],sum2,sum3,sum4,width)||
-        helper(a,i-1,sum1,sum2+a[i],sum3,sum4,width)||
-        helper(a,i-1,sum1,sum2,sum3+a[i],sum4,width)||
-        helper(a,i-1,sum1,sum2,sum3,sum4+a[i],width);
     }
 }
