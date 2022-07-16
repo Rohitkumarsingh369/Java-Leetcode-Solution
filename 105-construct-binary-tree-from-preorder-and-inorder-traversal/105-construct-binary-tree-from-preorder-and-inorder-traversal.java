4de12fd0d8b2e665a297fa2cbcbc0ab6d5524104
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-16 17:57:45 +0530

    Time: 4 ms (64.48%), Space: 43.3 MB (83.83%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,28 +15,23 @@
  */
 
 class Solution {
-     int preindex=0;
-   
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        preindex=0;
-        HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
-        for(int i=0;i<inorder.length;i++)
-            hm.put(inorder[i],i);
-        
-       return  buildtree(preorder,inorder,0,preorder.length-1,hm);
+     public TreeNode buildTree(int[] preorder, int[] inorder) {
+    return helper(0, 0, inorder.length - 1, preorder, inorder);
+}
+
+public TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+    if (preStart > preorder.length - 1 || inStart > inEnd) {
+        return null;
     }
-    public TreeNode buildtree(int [] preorder,int [] inorder,int start,int end,HashMap<Integer,Integer> hm){
-        if(start>end)
-            return null;
-        
-        
-        int inindex=hm.get(preorder[preindex++]);
-        
-        TreeNode root=new TreeNode(inorder[inindex]);
-        
-        root.left=buildtree(preorder,inorder,start,inindex-1,hm);
-        root.right=buildtree(preorder,inorder,inindex+1,end,hm);
-        
-        return root;
+    TreeNode root = new TreeNode(preorder[preStart]);
+    int inIndex = 0; // Index of current root in inorder
+    for (int i = inStart; i <= inEnd; i++) {
+        if (inorder[i] == root.val) {
+            inIndex = i;
+        }
     }
+    root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
+    root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd, preorder, inorder);
+    return root;
+}
 }
