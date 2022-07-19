1eba1f4dc7d25265cf4dbb1cdae39069cf36369d
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-19 14:28:57 +0530

    Time: 4 ms (81.18%), Space: 44.2 MB (50.11%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -6,7 +6,7 @@ class Solution {
             while (!stack.isEmpty() && stack.peek() < num)
                 map.put(stack.pop(), num);
             stack.push(num);
-        }   
+        }    
         for (int i = 0; i < findNums.length; i++)
             findNums[i] = map.getOrDefault(findNums[i], -1);
         return findNums;
