cbac54bd2843c6e8aa14450609c3c90a53d51922
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-22 12:58:25 +0530

    Time: 2 ms (99.28%), Space: 47.4 MB (72.73%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -23,14 +23,13 @@ class Solution {
             {
                 ListNode node = new ListNode(sum%10);
                 temp.next = node;
-                temp = temp.next;
                 carry = 1; //if sum>9, then carry is always 1 because the numbers in both the linked list is between 0 and 9
             }else{
                 ListNode node = new ListNode(sum);
                 temp.next = node;
-                temp = temp.next;
                 carry = 0; //because sum<=9
             }
+            temp=temp.next;
             if(temp1!=null)temp1 = temp1.next;
             if(temp2!=null)temp2 = temp2.next;
         }
