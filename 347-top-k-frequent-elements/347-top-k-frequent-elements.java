3136d69cf868813a33ce3a24f1f2418c5e4474f9
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-24 03:14:22 +0530

    Time: 26 ms (22.43%), Space: 51.1 MB (20.28%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,17 +1,18 @@
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    
+   /* public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> countMap = new HashMap<>();
-    List<Integer> ret = new ArrayList<>();
-    for (int n : nums) {
-        if (countMap.containsKey(n)) {
-            countMap.put(n ,countMap.get(n)+1);
-        } else {
-            countMap.put(n ,1);
-        }
-    }
-   PriorityQueue<Map.Entry<Integer, Integer>> pq =
-            new PriorityQueue<Map.Entry<Integer, Integer>>((o1, o2) -> o2.getValue() - o1.getValue());
-    pq.addAll(countMap.entrySet());
+    
+            for (int n : nums) {
+                if (countMap.containsKey(n)) {
+                    countMap.put(n ,countMap.get(n)+1);
+                } else {
+                    countMap.put(n ,1);
+                }
+            }
+           PriorityQueue<Map.Entry<Integer, Integer>> pq =
+                    new PriorityQueue<Map.Entry<Integer, Integer>>((o1, o2) -> o2.getValue() - o1.getValue());
+            pq.addAll(countMap.entrySet());
 
     int result[]=new int[k];
     //List<Integer> ret = new ArrayList<>();
@@ -19,5 +20,29 @@ class Solution {
         result[i]=(pq.poll().getKey()).intValue();
     }
     return result;
+    }*/
+    
+    public int[] topKFrequent(int[] nums, int k){
+        Map<Integer,Integer> countmap=new HashMap<>();
+        for(int num:nums){
+            if(countmap.containsKey(num)){
+                countmap.put(num,countmap.get(num)+1);
+            }
+            else{
+                countmap.put(num,1);
+            }
+        }
+        
+        PriorityQueue<Map.Entry<Integer,Integer>> top=new PriorityQueue<
+            Map.Entry<Integer,Integer>>((o1,o2)->o2.getValue()-o1.getValue());
+        
+        top.addAll(countmap.entrySet());
+        int result[]=new int[k];
+
+        for(int i=0;i<k;i++){
+            result[i]=top.poll().getKey().intValue();
+        }
+        
+        return result;
     }
 }
