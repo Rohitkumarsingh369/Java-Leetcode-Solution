62af449ec02af3d11379cb22da332958cd837d0c
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-24 06:43:34 +0530

    Time: 93 ms (48.98%), Space: 135.6 MB (21.12%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,59 +1,17 @@
-class LRUCache {
+/*class LRUCache {
 
-    class Node{
-        Node prev,next;
-        int key,value;
-        Node(int key,int value){
-            this.key=key;
-            this.value=value;
-        }
-    }
-    int capacity;
-        
-    Node head=new Node(0,0),tail=new Node(0,0);
-    HashMap<Integer,Node> map=new HashMap<>();
     public LRUCache(int capacity) {
-        this.capacity=capacity;
-        head.next=tail;
-        tail.prev=head;
+        
     }
     
     public int get(int key) {
-        if(map.containsKey(key)){
-            Node node=map.get(key);
-            remove(node);
-            insert(node);
-            return node.value;
-        }
-        return -1;
+        
     }
     
     public void put(int key, int value) {
-        if(map.containsKey(key))
-            remove(map.get(key));
         
-        if(map.size()==capacity)
-            remove(tail.prev);
-        
-        insert(new Node(key,value));
     }
-    
-    public void remove(Node node){
-        map.remove(node.key);
-        
-        node.prev.next=node.next;
-        node.next.prev=node.prev;
-    }
-    
-    public void insert(Node node){
-        map.put(node.key,node);
-        
-        node.next=head.next;
-        node.next.prev=node;
-        head.next=node;
-        node.prev=head;
-    }
-}
+}*/
 
 /**
  * Your LRUCache object will be instantiated and called as such:
@@ -61,3 +19,67 @@ class LRUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+class LRUCache
+{
+     class Node {
+		Node next,prev;
+		int key,value;
+		public Node(int key, int value) {
+			this.key = key;
+			this.value = value;
+		}	
+	} 
+	
+	   int cap;
+	   Node head=new Node(0,0);
+	   Node tail=new Node(0,0);
+	   HashMap<Integer,Node> map;
+	   LRUCache(int cap)
+	    {
+	    	this.cap=cap;
+	    	map=new HashMap<>();
+	    	head.next=tail;
+	    	tail.prev=head;
+	    }
+	    
+
+	    private void insert(Node node) {
+	    	map.put(node.key, node);
+			Node headnext=head.next;
+			head.next=node;
+			node.prev=head;
+			node.next=headnext;
+			headnext.prev=node;
+		}
+		
+		private void remove(Node node) {
+			map.remove(node.key);
+			node.next.prev=node.prev;
+			node.prev.next=node.next;
+		}
+		
+    public int get(int key)
+    {
+        	if(map.containsKey(key)) {
+	        	Node node=map.get(key);
+	        	remove(node);
+	        	insert(node);
+	        	return node.value;
+	        	
+	        }else {
+	        	return -1;
+	        }
+    }
+   
+    public void put(int key, int value)
+    {
+        	if(map.containsKey(key)) {
+	        	remove(map.get(key));	        	
+	        }
+	        
+	        if(cap==map.size()) 	        	
+	        	remove(tail.prev);
+	        
+	        insert(new Node(key,value));
+    }
+}
