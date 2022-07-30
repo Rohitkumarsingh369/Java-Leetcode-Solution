78db43c0a61f4980154711d7ca2db6e78cac75f7
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-07-30 10:39:51 +0530

    Time: 26 ms (86.18%), Space: 88.3 MB (60.16%) - LeetHub

---
+++ 916-word-subsets/916-word-subsets.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> wordSubsets(String[] A, String[] B) {
+        List<String> result=new ArrayList<>();
+        int target[]=new int[26];
+        for(String word:B){
+            int temp[]=new int[26];
+            for(char ch:word.toCharArray()){
+                temp[ch-'a']++;
+                target[ch-'a']=Math.max(temp[ch-'a'],target[ch-'a']);
+            }
+        }
+        
+        for(String word:A){
+            int source[]=new int[26];
+            for(char ch:word.toCharArray()){
+                source[ch-'a']++;
+            }
+            if(subset(source,target))
+                result.add(word);
+        }
+        return result;
+    }
+    public boolean subset(int []parent,int []child){
+        for(int i=0;i<26;i++){
+            if(parent[i]<child[i])
+                return false;
+        }
+        return true;
+    }
+}
