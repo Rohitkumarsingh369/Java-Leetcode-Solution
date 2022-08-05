f0b5fe5dcde7c1e8dbf94fa88749aff4ea0efbf5
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-08-05 19:36:17 +0530

    Time: 1 ms (86.92%), Space: 43.5 MB (73.61%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -1,24 +1,25 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int left=0,right=nums.length-1;
+        int left = 0, right =  nums.length-1, mid;
         
-        while(left<=right){
-            if(nums[left]==target || nums[right]==target)
-                return true;
-            else if(nums[left]<target){
-                while(left<right && nums[left+1]==nums[left])
-                    left++;
-                
-                left++;
+        while(left<=right)
+        {
+            mid = (left + right) >> 1;
+            if(nums[mid] == target) return true;
+
+            // the only difference from the first one, trickly case, just updat left and right
+            if( (nums[left] == nums[mid]) && (nums[right] == nums[mid]) ) {++left; --right;}
+
+            else if(nums[left] <= nums[mid])
+            {
+                if( (nums[left]<=target) && (nums[mid] > target) ) right = mid-1;
+                else left = mid + 1; 
             }
-            else if(nums[right]>target){
-                while(left<right && nums[right-1]==nums[right])
-                    right--;
-            
-                right--;
-            }   
             else
-                break;
+            {
+                if((nums[mid] < target) &&  (nums[right] >= target) ) left = mid+1;
+                else right = mid-1;
+            }
         }
         return false;
     }
