0dd0cd7f54e6d83f0b24c42efcb7ebe15597ae87
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-08-13 04:18:55 +0530

    Time: 32 ms (84.96%), Space: 61.5 MB (80.80%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.java
@@ -1,22 +1,17 @@
 class Solution {
     public long numberOfWays(String s) {
-       long n0 = 0, n1 = 0, n01 = 0, n10 = 0, n010 = 0, n101 = 0;
-        for(char c: s.toCharArray())
-        {
-            if(c == '0')
-            {
-                n0++;
-                n10 += n1;
-                n010 += n01;
-            }
-            else
-            {
-                n1++;
-                n01 += n0;
-                n101 += n10;
+        long one = 0, zero = 0, oneZero = 0, zeroOne = 0, ways = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '0') {
+                ++zero;
+                oneZero += one; // Count in '10'.
+                ways += zeroOne; // Count in '010'.
+            }else {
+                ++one;
+                zeroOne += zero; // Count in '01'.
+                ways += oneZero; // Count in '101'.
             }
         }
-        
-        return n101 + n010; 
+        return ways;
     }
 }
