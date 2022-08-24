52d80974ddbc89a521c1ea37963d10509258a56c
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-08-24 21:15:56 +0530

    Time: 22 ms (62.48%), Space: 47.5 MB (38.69%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0)
+            return false;
+        /* The maximum power of 3 value that
+           integer can hold is 1162261467 ( 3^19 ) .*/
+        return 1162261467 % n == 0;
+    }
+}
