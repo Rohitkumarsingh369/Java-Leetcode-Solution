952b779b5334a40aeadee1b768d267f771442431
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-08-24 12:23:07 +0530

    Time: 0 ms (100.00%), Space: 59.9 MB (77.31%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -7,27 +7,21 @@ class Solution {
         }
         return result;*/
         
-        int left = 0;
-        int right = nums.length-1;
+        int low = 0;
+        int high = nums.length-1;
         
-        while(left < right){
+        while(low < high){
             
-            int mid = (left+right)/2;
-            
-            if(nums[mid] == nums[mid+1]){
-                mid = mid - 1;                   //two same elements should be in same half
+            int mid = (low+high)/2;
+            if(nums[mid] == nums[mid^1]){
+                low = mid+1;
+            }
+            else{
+                high = mid;
             }
             
-            if((mid-left+1)%2 != 0){
-                right = mid;                     // checking the length of left half. 
-                                                //If its is odd then update ur right pointer to mid
-            }else{
-                left = mid + 1;                  // else your right half will be odd 
-                                                //then update your left pointer to mid+1
-
-            }
+            
         }
-        
-        return nums[left];                  //left pointer will have the answer at last 
+        return nums[low];
     }
 }
