527aa06c39ece6b46cd9fdd9974505df6ae62453
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-08-26 13:37:30 +0530

    Time: 33 ms (58.90%), Space: 142.7 MB (19.89%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,5 +1,49 @@
 class Solution {
-   /* int n,m;
+    int n,m;
+    int[] dx={1,-1,0,0};
+    int[] dy={0,0,1,-1};
+    int ans=1;
+    void dfs(int x,int y,int[][] g1,int[][] g2)
+    {
+        g2[x][y]=0;
+        ans*=g1[x][y];
+        for(int i=0;i<4;i++)
+        {
+            int ind1=x+dx[i];
+            int ind2=y+dy[i];
+            if(ind1<0 || ind2<0 || ind1>=n || ind2>=m)
+                continue;
+            if(g2[ind1][ind2]==0)
+                continue;
+            if(g2[ind1][ind2]==1)
+                dfs(ind1,ind2,g1,g2);
+                
+       }
+        
+        
+    }
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        n=grid1.length;
+        m=grid1[0].length;
+        int i,j,cnt=0;
+        
+         for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(grid2[i][j]==1)
+                {
+                     ans=1;
+                    dfs(i,j,grid1,grid2);
+                    if(ans==1)
+                    {cnt++;
+                    }
+                }
+            }
+        }
+        return cnt;
+    }
+    /*int n,m;
     int[] dx={1,-1,0,0};
     int[] dy={0,0,1,-1};
     int ans=1;
@@ -48,7 +92,7 @@ class Solution {
         return cnt;
     }*/
     
-    public int countSubIslands(int[][] grid1, int[][] grid2) {
+    /*public int countSubIslands(int[][] grid1, int[][] grid2) {
         int m = grid1.length;
         int n = grid1[0].length;
         int count = 0;
@@ -89,5 +133,5 @@ class Solution {
         }
         
         return count;
-    }
+    }*/
 }
