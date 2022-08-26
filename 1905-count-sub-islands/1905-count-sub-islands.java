ae891600c5ce99632f742df3662b2f0532178c88
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-08-26 12:59:32 +0530

    Time: 52 ms (18.41%), Space: 132.1 MB (40.76%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.java
+++ 1905-count-sub-islands/1905-count-sub-islands.java
@@ -1,5 +1,5 @@
 class Solution {
-    int n,m;
+   /* int n,m;
     int[] dx={1,-1,0,0};
     int[] dy={0,0,1,-1};
     int ans=1;
@@ -46,5 +46,48 @@ class Solution {
             }
         }
         return cnt;
+    }*/
+    
+    public int countSubIslands(int[][] grid1, int[][] grid2) {
+        int m = grid1.length;
+        int n = grid1[0].length;
+        int count = 0;
+        int[] dir = {1, 0, -1, 0, 1};
+        
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(grid2[i][j] == 0 )
+                    continue;
+                
+                Queue<int[]> queue = new LinkedList<>();
+                boolean flag = true;
+                grid2[i][j] = 0;
+                
+                queue.add(new int[] {i, j});
+                
+                while(!queue.isEmpty()) {
+                    int[] vtx = queue.remove();
+                    
+                    if(grid1[vtx[0]][vtx[1]] == 0)
+                        flag = false;
+                    
+                    for(int k = 0; k < 4; ++k) {
+                        int x = vtx[0] + dir[k];
+                        int y = vtx[1] + dir[k + 1];
+                        
+                        if(x >= 0 && x < m && y >= 0 && y < n && grid2[x][y] == 1) {
+                            grid2[x][y] = 0;
+                            
+                            queue.add(new int[] {x, y});
+                        }
+                    }
+                }
+                
+                if(flag)
+                    ++count;
+            }
+        }
+        
+        return count;
     }
 }
