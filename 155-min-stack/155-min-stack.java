12d9044f29fa65b2a1c8e3b49a034bb850e5f2a3
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-09-20 16:35:12 +0530

    Time: 7 ms (71.96%), Space: 49.1 MB (9.83%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,42 +1,29 @@
 class MinStack {
-
-    int minelement;
-    Stack<Integer> stack;
+    Stack<Integer> stk=new Stack<>();
+    int min = Integer.MAX_VALUE;
     public MinStack() {
-        stack=new Stack<>();
+        
     }
-    
     public void push(int val) {
-        if(stack.isEmpty())
-            minelement=Integer.MAX_VALUE;
-        
-        if(val<minelement)
-            minelement=val;
-        
-        stack.push(val);
-        stack.push(minelement);
+        if(min>=val){
+            stk.push(min);
+            min = val;
+        }
+        stk.push(val);
     }
     
     public void pop() {
-        stack.pop();
-        stack.pop();
         
-        if(!stack.isEmpty())
-            minelement=stack.peek();
+        if(stk.pop()==min){
+            min = stk.pop();
+        }            
     }
     
-    public int top() {
-        int remain=stack.pop();
-        int top=stack.peek();
-        stack.push(remain);
-        return top;
+    public int top() {       
+        return stk.peek();
     }
-    
     public int getMin() {
-    
-        if(stack.isEmpty())
-            return -1;
-        return stack.peek();
+        return min;
     }
 }
 
