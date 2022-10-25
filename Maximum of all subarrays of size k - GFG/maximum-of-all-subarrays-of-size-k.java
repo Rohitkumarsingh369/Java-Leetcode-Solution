0747a492942a872961c7a5cef546fb4f0acbf2d3
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-10-25 22:09:01 +0530

    Added solution - LeetHub

---
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.java
@@ -0,0 +1,100 @@
+// { Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Main
+{
+    public static void main(String args[])
+    {
+        //taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+        
+        //taking total count of testcases
+        int t = sc.nextInt();
+        
+        
+        
+        while(t-- > 0)
+        {
+            //taking total number of elements
+            int n = sc.nextInt();
+            
+            //taking size of subArrays 
+            int k = sc.nextInt();
+            
+            //Declaring and Intializing an array of size n
+            int arr[] = new int[n];
+            
+            //adding all the elements to the array 
+            for(int i = 0; i <n; i++)
+            {
+                arr[i] =sc.nextInt();
+            }
+            
+            //Calling the method max_of_subarrays of class solve
+            //and storing the result in an ArrayList
+            ArrayList <Integer> res = new Solution().max_of_subarrays(arr, n, k);
+            
+            //printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++)
+                System.out.print (res.get (i) + " ");
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        // Your code here
+        ArrayList<Integer> a = new ArrayList<Integer>();
+
+        int max = arr[0];
+
+        int maxIndex = 0;
+
+        for (int i = 0; i <= (n - k); i++)
+
+        {
+
+            if (i > maxIndex)
+
+            {
+
+                max = arr[i];
+
+                maxIndex = i;
+
+            }
+
+            for (int j = maxIndex + 1; j < (i + k); j++)
+
+            {
+
+                if (max < arr[j])
+
+                {
+
+                    max = arr[j];
+
+                    maxIndex = j;
+
+                }
+
+            }
+
+            a.add(max);
+
+        }
+
+        return a;
+    }
+}
