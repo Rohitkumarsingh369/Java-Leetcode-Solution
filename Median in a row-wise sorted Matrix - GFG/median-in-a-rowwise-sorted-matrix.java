2763eab7cb5229b0a63544edff60176244011bfd
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-11-01 22:07:34 +0530

    Added solution - LeetHub

---
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S[] = read.readLine().split(" ");
+            int R = Integer.parseInt(S[0]);
+            int C = Integer.parseInt(S[1]);
+            int matrix[][] = new int[R][C];
+            int c = 0;
+            for(int i = 0; i < R; i++){
+                String line[]=read.readLine().trim().split(" ");
+                for(int j = 0; j < C; j++){
+                    matrix[i][j] = Integer.parseInt(line[j]);
+                }
+            }
+            Solution ob = new Solution();
+            int ans = ob.median(matrix, R, C);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    int median(int matrix[][], int R, int C) {
+        // code here  
+         int a[] = new int[R*C];
+
+       int size =0;
+
+       for(int i =0; i<R;i++){
+
+           for(int j =0; j<C;j++){
+
+               a[size++] = matrix[i][j];
+
+           }
+
+       }
+
+       
+
+       Arrays.sort(a);
+
+       
+
+       return( a[(((R*C)+1)/2)-1]);
+    }
+}
