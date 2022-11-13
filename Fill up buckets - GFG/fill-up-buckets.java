99f9ec9c555e9d41a8f2dfdd9caa7dfaf1fb26bf
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2022-11-13 22:10:17 +0530

    Added solution - LeetHub

---
+++ Fill up buckets - GFG/fill-up-buckets.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String s = br.readLine().trim();
+            String[] S = s.split(" ");
+            int[] capacity = new int[n];
+            for(int i = 0; i < n; i++)
+                capacity[i] = Integer.parseInt(S[i]);
+            Solution ob = new Solution();
+            int ans = ob.totalWays(n, capacity);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public int totalWays(int n, int[] capacity) {
+        // code here
+         long ans = 1, mod = 1000000007;
+
+        Arrays.sort(capacity);
+
+        for(int i=0; i< n; i++){
+
+            ans = (ans * (capacity[i] - i))%mod;
+
+        }
+
+        return (int)ans;
+    }
+}
