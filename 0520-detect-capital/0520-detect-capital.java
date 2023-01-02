f13dc8d849d56110a6ab062c1e99879cf60d55ac
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-01-02 23:04:52 +0530

    Time: 2 ms (69.88%), Space: 41.6 MB (67.55%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if(word.length() < 2) return true;
+        if(Character.isUpperCase(word.charAt(0)) && Character.isUpperCase(word.charAt(1))){
+            for(int i = 2; i < word.length(); i++){
+                if(Character.isLowerCase(word.charAt(i))) return false;
+            }
+        }
+        else{
+            for(int i = 1; i < word.length(); i++){
+                if(Character.isUpperCase(word.charAt(i))) return false;
+            }
+        }
+        return true;
+    }
+}
