c4b3dba4093928224f8921c3e73514dad1ea58ee
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-01-12 22:56:15 +0530

    Added solution - LeetHub

---
+++ Minimize the sum - GFG/minimize-the-sum.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution {
+    long minimizeSum(int N, int arr[]) {
+        // code here
+        PriorityQueue<Long> q=new PriorityQueue<>();
+        for(int i:arr){
+            q.offer((long)i);
+        }
+        long sum=0,a=0,b=0;
+        
+        while(q.size()>1){
+            sum+=(a=q.poll())+(b=q.poll());
+            q.offer(a+b);
+            // System.out.println(sum+" "+q.peek());
+        }
+        return sum;
+    }
+}
+
+//{ Driver Code Starts.
+
+public class GFG
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        while (t-- > 0)
+        {
+            int n = sc.nextInt();
+            int A[] = new int[n];
+            
+            for (int i = 0; i < n;i++)
+            {
+                A[i] = sc.nextInt();
+            }
+            Solution obj = new Solution();
+            long ans = obj.minimizeSum(n, A);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
