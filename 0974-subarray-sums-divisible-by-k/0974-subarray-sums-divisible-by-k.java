edca8390e72958f96709bc6139edef215c0d06a7
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-01-19 22:20:35 +0530

    Time: 3 ms (98.01%), Space: 45.7 MB (96.39%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int subarraysDivByK(int[] a, int k) {
+       int mod[]=new int[k];
+        Arrays.fill(mod,0);
+        
+        int cumsum=0;
+        int n=a.length;
+        for(int i=0;i<n;i++){
+            cumsum+=a[i];
+            mod[((cumsum%k)+k)%k]++;
+        }
+        int result=0;
+        for(int i=0;i<k;i++){
+            if(mod[i]>1)
+            result+=(mod[i]*(mod[i]-1))/2;
+        }
+        result+=mod[0];
+        
+        /*for(int i=0;i<k;i++){
+            System.out.print(mod[i]+" ");
+        }*/
+        return result;
+    }
+}
