f2213bcd043d088f807ef5cb3a24f545cf435421
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-01-25 22:39:46 +0530

    Added solution - LeetHub

---
+++ Type it! - GFG/type-it.java
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t = Integer.parseInt(read.readLine());
+
+        while (t-- > 0) {
+            String s = read.readLine().trim();
+            Solution ob = new Solution();
+            int ans = ob.minOperation(s);
+            out.println(ans);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+     int minOperation(String s) {
+        int ans=s.length();
+        char[] c=s.toCharArray();
+        for(int i=0;i<s.length()/2;i++) {
+           if(s.substring(0,i+1).equals(s.substring(i+1,i+i+2)))ans = s.length()-i;
+        }
+        return ans;
+    }
+}
