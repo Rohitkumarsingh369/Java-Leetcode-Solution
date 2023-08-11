a5ac99e467ee3baef346af2d1b86a990523ad5a1
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-08-11 13:01:34 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.java
+++ Coin Change - GFG/coin-change.java
@@ -1,4 +1,4 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 // Initial Template for Java
 
 import java.io.*;
@@ -8,50 +8,44 @@ class GfG {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while (t-- > 0) {
-            int n = sc.nextInt();
-            int m = sc.nextInt();
-            int Arr[] = new int[m];
-            for (int i = 0; i < m; i++) Arr[i] = sc.nextInt();
+            int sum = sc.nextInt();
+            int N = sc.nextInt();
+            int coins[] = new int[N];
+            for (int i = 0; i < N; i++) coins[i] = sc.nextInt();
             Solution ob = new Solution();
-            System.out.println(ob.count(Arr, m, n));
+            System.out.println(ob.count(coins, N, sum));
         }
     }
-}// } Driver Code Ends
+}
+
+
+// } Driver Code Ends
 
 
 // User function Template for Java
 
 class Solution {
-    public long count(int a[], int m, int n) {
-        // code here.
-        
-        
-        long dp[]=new long[n+1];
-        
-        dp[0]=1;
-        for(int i=0;i<m;i++)//size of the infinity supply given to us
-        {
-            for(int j=a[i];j<=n;j++) //form the particular supply till the number given to us
-            {
-                dp[j]+=dp[j-a[i]];
-            }
+    public long coinChange(int ind, int Amount, int [] coins, long [][] dp){
+        if(ind == coins.length){
+            if(Amount == 0) return 1;
+            return 0;
+        } 
+        if(dp[ind][Amount] !=-1) return dp[ind][Amount];
+        long include=0, notinclude = 0;
+        if(Amount >= coins[ind]){
+            include = coinChange(ind, Amount-coins[ind], coins, dp);
         }
-        
-        return dp[n];
-        
-        
-        
-        /*long coin[]=new long[n+1];
-        
-        coin[0]=1;
-        for(int i=0;i<m;i++){
-            
-            for(int j=a[i];j<=n;j++){
-                
-                coin[j]+=coin[j-a[i]];
-            }
+            notinclude = coinChange(ind+1, Amount, coins, dp);
+
+            return dp[ind][Amount] = include + notinclude;
+    }
+    
+    public long count(int coins[], int N, int sum) {
+        long [][]dp = new long[N][sum+1];
+        for(int i=0; i<dp.length; i++){
+            for(int j=0; j<dp[0].length; j++) dp[i][j] = -1;
         }
-        
-        return coin[n];*/
+
+        return coinChange(0, sum, coins, dp);
     }
 }
