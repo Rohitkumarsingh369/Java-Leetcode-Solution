9177d48f176b5e9f35a9dd80c10d5c5eef471e57
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-08-12 03:44:05 +0530

    Added solution - LeetHub

--- Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.java
+++ Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.java
@@ -45,30 +45,7 @@ class Solution
     //Function to check if two strings are rotations of each other or not.
     public static boolean areRotations(String s1, String s2 )
     {
-        // Your code here
-        if (s1.length() != s2.length()) {
-            return false;
-        }
-        else {
-            for (int i = 0; i < s1.length(); i++) {
-                // checking character at ith index with
-                // first character of s2
-                if (s1.charAt(i) == s2.charAt(0)) {
-                    // checking prefix of s2 with suffix of
-                    // s1
-                    if (s1.substring(i).equals(s2.substring(
-                            0, s1.length() - i))) {
-                        // checking prefix of s1 with suffix
-                        // of s2
-                        if (s1.substring(0, i).equals(
-                                s2.substring(s1.length()
-                                             - i)))
-                            return true;
-                    }
-                }
-            }
-        }
-        return false;
+        return ((s1.length()==s2.length())&&((s1+s1).indexOf(s2)!=-1));
     }
     
 }
