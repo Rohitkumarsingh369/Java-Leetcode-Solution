b914fdcff85fce173bea123fe58121f46e213b00
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-08-16 23:29:18 +0530

    Added solution - LeetHub

---
+++ Nth catalan number - GFG/nth-catalan-number.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            Solution obj = new Solution();
+            int res = obj.findCatalan(n);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static int findCatalan(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        int MOD = 1000000007;
+        int[] catalan = new int[n + 1];
+        catalan[0] = 1;
+        catalan[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            catalan[i] = 0;
+            for (int j = 0; j < i; j++) {
+                catalan[i] = (int) ((catalan[i] + (long) catalan[j] * catalan[i - j - 1]) % MOD);
+            }
+        }
+
+        return catalan[n];
+    }
+}
