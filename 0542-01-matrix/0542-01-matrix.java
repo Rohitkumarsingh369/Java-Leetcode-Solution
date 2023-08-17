badefaa768a9b57ba44a8b225924aac045ee0f9e
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-08-18 00:02:34 +0530

    Time: 11 ms (85.91%), Space: 47 MB (8.99%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,23 @@
+class Solution {
+    int[] DIR = new int[]{0, 1, 0, -1, 0};
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length, n = mat[0].length; // The distance of cells is up to (M+N)
+        Queue<int[]> q = new ArrayDeque<>();
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if (mat[r][c] == 0) q.offer(new int[]{r, c});
+                else mat[r][c] = -1; // Marked as not processed yet!
+
+        while (!q.isEmpty()) {
+            int[] curr = q.poll();
+            int r = curr[0], c = curr[1];
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + DIR[i], nc = c + DIR[i+1];
+                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
+                mat[nr][nc] = mat[r][c] + 1;
+                q.offer(new int[]{nr, nc});
+            }
+        }
+        return mat;
+    }
+}
