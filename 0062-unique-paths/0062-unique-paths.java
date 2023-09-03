92fc360eea82b2e002f2155d70fa3339b81519b3
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-09-03 11:07:38 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (94.44%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        /*int dp[][]=new int[m][n];
+        
+        for(int i=0;i<n;i++)//fill last row with 1
+        {
+            dp[m-1][i]=1;
+        }
+        for(int i=0;i<m;i++)//fill last column with 1
+        {
+            dp[i][n-1]=1;
+        }
+        
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+       return  dp[0][0];*/
+        
+        
+        int dp[]=new int[n];
+        
+        for(int i=0;i<n;i++){
+            dp[i]=1;
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[j]+=dp[j-1];
+            }
+        }
+        
+        return dp[n-1];
+    }
+}
