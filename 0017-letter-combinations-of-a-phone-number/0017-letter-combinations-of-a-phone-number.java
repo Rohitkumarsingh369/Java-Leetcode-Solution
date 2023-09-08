0847f230969196cadb8a08cc79307fbe2d0b1fea
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-09-08 10:55:09 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (76.15%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,32 +1,27 @@
-/*Here's my Solution with the time Complexity O(K^N) where N is digit length example :- "23" & K is no of character present in a string example :- "abc"*/
-class Solution {
-    public List<String> letterCombinations(String digits) {
-        List<String> res = new ArrayList<>();
-        if(digits.length() == 0) return new ArrayList<>();
-        Map<String, String> map = new HashMap<>();
-        
-        map.put("2", "abc");map.put("3", "def");map.put("4", "ghi");
-        map.put("5", "jkl");map.put("6", "mno");map.put("7", "pqrs");
-        map.put("8", "tuv");map.put("9", "wxyz");
-        
-        Deque<String> dq = new ArrayDeque<>();
-        dq.add("");
-        for(int i = 0; i < digits.length(); i++){ // 2 3 4
-            String d = digits.substring(i, i + 1); // 2
-            String fromMap = map.get(d); // abc
-            int n = dq.size();
-            for(int j = 0; j < n; j++){
-                String pull = dq.pollFirst(); // [] [a][b][c]
-                for(int k = 0; k < fromMap.length(); k++){
-                    String x = pull.concat(fromMap.charAt(k) + ""); // "" + a
-                    dq.add(x);
-                }
-            }
+class Solution 
+{
+    List<String> a = new ArrayList<>();
+    String arr[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digits) 
+    {
+        if(digits.equals(""))
+            return a;
+        func(0,new StringBuilder(""),digits);
+        return a;
+    }
+    void func(int index,StringBuilder s,String digits)
+    {
+        if(s.length()==digits.length())
+        {
+            a.add(new String(s));
+            return;
         }
-        
-        while(!dq.isEmpty()){
-            res.add(dq.poll());
+        int a = digits.charAt(index) - '0';
+        for(int i=0;i<arr[a].length();i++)
+        {
+            s.append((arr[a].charAt(i)));
+            func(index+1,s,digits);
+            s.deleteCharAt(s.length()-1);
         }
-        return res;
     }
 }
