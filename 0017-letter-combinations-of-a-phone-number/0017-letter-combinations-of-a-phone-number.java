5fbf6d10d819801fd921fbfc903397f657c6f0e7
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-09-08 11:20:48 +0530

    Time: 1 ms (78.85%), Space: 40.9 MB (85.66%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,14 +1,16 @@
 class Solution 
 {
-    List<String> a = new ArrayList<>();
-    String arr[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    public List<String> letterCombinations(String digits) 
+    List<String> result = new ArrayList<>();
+    String mapping[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    /*public List<String> letterCombinations(String digits) 
     {
         if(digits.equals(""))
             return a;
         func(0,new StringBuilder(""),digits);
         return a;
     }
+    
     void func(int index,StringBuilder s,String digits)
     {
         if(s.length()==digits.length())
@@ -22,6 +24,28 @@ class Solution
             s.append((arr[a].charAt(i)));
             func(index+1,s,digits);
             s.deleteCharAt(s.length()-1);
+        }
+    }*/
+    public List<String> letterCombinations(String digits) {
+        
+        if(digits.length()==0)
+            return result;
+        dfs(0,digits,new StringBuilder());
+        return result;
+    }
+    
+    void dfs(int length,String digits, StringBuilder temp){
+        if(length==digits.length()){
+            result.add(temp.toString());
+            return;
+        }
+        
+        char ch=digits.charAt(length);
+        String str=mapping[ch-'0'];
+        for(char c:str.toCharArray()){
+            temp.append(c);
+            dfs(length+1,digits,temp);
+            temp.deleteCharAt(temp.length()-1);
         }
     }
 }
