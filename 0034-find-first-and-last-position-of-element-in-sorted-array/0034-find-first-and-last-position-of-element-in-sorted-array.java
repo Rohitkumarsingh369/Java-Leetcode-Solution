1c652daceee0b3fcb8c66fa39d169a4308aa7ad4
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-10-04 00:54:56 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (25.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,37 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int res[]=new int[]{-1,-1};
+        int left=findLeftBound(nums,target);
+        int right=findRightBound(nums,target);
         
-        if(nums.length==0)
-            return res;
+        return new int[]{left,right};
+    }
+    private int findLeftBound(int[] nums,int target){
+        int index=-1,low=0,high=nums.length-1;
         
-        int start=0,end=nums.length-1;
-        
-        while(start<end){
-            int mid=start+(end-start)/2;
-            
-            if(nums[mid]>=target)
-                end=mid;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                index=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]<target)
+                low=mid+1;
             else
-                start=mid+1;
+                high=mid-1;
         }
         
-        if(nums[start]!=target)
-            return res;
+        return index;
+    }
+    private int findRightBound(int[] nums,int target){
+        int index=-1,low=0,high=nums.length-1;
         
-        res[0]=start;
-        end=nums.length;
-        
-        while(start<end){
-            int mid=start+(end-start)/2;
-            
-            if(nums[mid]>target)
-                end=mid;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                index=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<target)
+                low=mid+1;
             else
-                start=mid+1;
+                high=mid-1;
         }
-        res[1]=start-1;
         
-        return res;
+        return index;
     }
 }
