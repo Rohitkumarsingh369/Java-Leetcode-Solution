5c951bafc41022eba7596bc66ed3792e792a018d
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-10-04 13:46:49 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (62.43%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -15,12 +15,12 @@ class Solution {
 
             else if(nums[left] <= nums[mid])
             {
-                if( (nums[left]<=target) && (nums[mid] > target) ) right = mid-1;
+                if( (nums[left]<=target) && (nums[mid] >= target) ) right = mid-1;
                 else left = mid + 1; 
             }
             else
             {
-                if((nums[mid] < target) &&  (nums[right] >= target) ) left = mid+1;
+                if((nums[mid] <= target) &&  (nums[right] >= target) ) left = mid+1;
                 else right = mid-1;
             }
         }
