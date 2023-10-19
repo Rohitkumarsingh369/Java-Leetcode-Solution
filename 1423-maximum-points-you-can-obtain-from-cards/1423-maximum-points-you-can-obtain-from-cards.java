12a3555a38008637c7dab0528a98aa90fb41249e
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-10-19 19:48:55 +0530

    Time: 2 ms (97.01%), Space: 55 MB (79.91%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,23 +1,24 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
-    int sumfront = 0;
-    int sumback = 0;
-    int max = 0;
-    
-    int front = 0,back = cardPoints.length-1;
-    
-    for(int i =0;i<k;i++){ //get sum of just the front
-        sumfront += cardPoints[front++];
-    }
-    max = sumfront; //put front sum as max
-	
-    for(int i=0;i<k;i++){ //get sum of (back + front) sum one by one
-        sumback += cardPoints[back--];
-        sumfront -= cardPoints[--front];
-        max = Math.max(max,sumback+sumfront); //check for max
-    }
-    
-    return max;
-    
-}
+        int sumfront = 0;
+        int sumback = 0;
+        int max = 0;
+
+        int front = 0,back = cardPoints.length-1;
+
+        for(int i =0;i<k;i++){ //get sum of just the front
+            sumfront += cardPoints[front++];
+        }
+        //System.out.println(sumfront);
+        max = sumfront; //put front sum as max
+
+        for(int i=0;i<k;i++){ //get sum of (back + front) sum one by one
+            sumback += cardPoints[back--];
+            sumfront -= cardPoints[--front];
+            //System.out.println(sumfront+" "+sumback);
+            max = Math.max(max,sumback+sumfront); //check for max
+        }
+
+        return max;
+  }
 }
