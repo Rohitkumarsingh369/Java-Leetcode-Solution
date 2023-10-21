79960069c0f3f44de69db12ed5c7de90d213c936
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-10-22 01:45:32 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (96.47%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,7 +1,7 @@
 //Runtime: 0 ms, faster than 100.00% of Java online submissions for Unique Paths II.
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m=obstacleGrid.length;
+       /* int m=obstacleGrid.length;
         if(m==0)
             return 0;
         int n=obstacleGrid[0].length;
@@ -38,6 +38,30 @@ class Solution {
             System.out.println();
         }
         
-       return  dp[m-1][n-1];
+       return  dp[m-1][n-1];*/
+        int row = obstacleGrid.length;
+        if (row == 0) return 0;
+        int col = obstacleGrid[0].length;
+        int[] dp = new int[col]; // One-dimensional array to store DP values
+        
+        // Initialize the first value of dp
+        dp[0] = (obstacleGrid[0][0] == 0) ? 1 : 0;
+        
+        // Initialize the first row of dp
+        for (int i = 1; i < col; i++) {
+            dp[i] = (obstacleGrid[0][i] == 0) ? dp[i - 1] : 0;
+        }
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[j] = 0; // Obstacle encountered, no paths from this point
+                } else if (j > 0) {
+                    dp[j] += dp[j - 1]; // Accumulate paths from the left
+                }
+            }
+        }
+        
+        return dp[col - 1];
     }
 }
