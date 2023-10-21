8af72ff797d1c88cbd8e6d192d37e63acaf7f8c7
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-10-21 18:58:49 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (81.10%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int rob(int[] hval) {
+        /*int n=hval.length;
+        if (n == 0)
+        return 0;
+        if (n == 1)
+            return hval[0];
+        if (n == 2)
+            return Math.max(hval[0], hval[1]);
+   
+        // dp[i] represent the maximum value stolen
+        // so far after reaching house i.
+        int[] dp = new int[n];
+   
+        // Initialize the dp[0] and dp[1]
+        dp[0] = hval[0];
+        dp[1] = Math.max(hval[0], hval[1]);
+   
+        // Fill remaining positions
+        for (int i = 2; i<n; i++)
+            dp[i] = Math.max(hval[i]+dp[i-2], dp[i-1]);
+   
+        return dp[n-1];*/
+        
+        
+        int length=hval.length;
+        if(length==0)
+            return 0;
+        if(length==1)
+            return hval[0];
+        if(length==2)
+            return Math.max(hval[0],hval[1]);
+        
+        hval[1]=Math.max(hval[0],hval[1]);
+        
+        for(int i=2;i<length;i++){
+            hval[i]=Math.max(hval[i-1],hval[i]+hval[i-2]);
+        }
+        
+        return hval[length-1];
+    }
+}
