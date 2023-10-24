a153e4a05ec132c023834c5c3e1cd577283f229e
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-10-25 00:30:18 +0530

    Time: 24 ms (47.53%), Space: 46.6 MB (22.97%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,18 +1,26 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
+        int n=nums.length;
          int result=0,sum=0;
         HashMap<Integer,Integer>hmap=new HashMap<Integer,Integer>();
-        for(int i:nums){
-            sum+=i;
-            if(sum==k){
-                result++;
-            }
-            if(hmap.containsKey(sum-k)){
-                int count=hmap.get(sum-k);
-                result+=count;
-            }
-            hmap.put(sum,hmap.getOrDefault(sum,0)+1);
+        int preSum = 0, cnt = 0;
+
+        hmap.put(0, 1); // Setting 0 in the map.
+        for (int i = 0; i < n; i++) {
+            // add current element to prefix Sum:
+            preSum += nums[i];
+
+            // Calculate x-k:
+            int remove = preSum - k;
+
+            // Add the number of subarrays to be removed:
+            cnt +=  hmap.getOrDefault(remove, 0);
+
+            // Update the count of prefix sum
+            // in the map.
+             hmap.put(preSum,  hmap.getOrDefault(preSum, 0) + 1);
         }
-        return result;
+        return cnt;
+        //return result;
     }
 }
