20a5d24c46b5903ae6c6f3266800fcc60f2dd170
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-10-26 22:56:07 +0530

    Time: 1 ms (100.00%), Space: 59.6 MB (34.98%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxSubArray(int[] a) {
-        int max_so_far=Integer.MIN_VALUE;
+        /*int max_so_far=Integer.MIN_VALUE;
         int n=a.length;
 		int max_ending_here=0;
 		int start=0,end=0,s=0;
@@ -18,6 +18,41 @@ class Solution {
 				s=i+1;
 			}
 		}
-        return max_so_far;
+        return max_so_far;*/
+        int n=a.length;
+        int maxSoFar=Integer.MIN_VALUE;
+        int maxEndingHere=0;
+        int start=0,end=0,s=0;
+        
+        for(int i=0;i<n;i++){
+            maxEndingHere+=a[i];
+            
+            if(maxSoFar<maxEndingHere){
+                maxSoFar=maxEndingHere;
+                end=i;
+                start=s;
+                    
+            }
+            if(maxEndingHere<0){
+                maxEndingHere=0;
+                s=i+1;
+            }
+        }
+        return maxSoFar;
     }
 }
+
+/*
+public int maxSubArray(int[] A) {
+        int n = A.length;
+        int[] dp = new int[n];//dp[i] means the maximum subarray ending with A[i];
+        dp[0] = A[0];
+        int max = dp[0];
+        
+        for(int i = 1; i < n; i++){
+            dp[i] = A[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.max(max, dp[i]);
+        }
+        
+        return max;
+}*/
