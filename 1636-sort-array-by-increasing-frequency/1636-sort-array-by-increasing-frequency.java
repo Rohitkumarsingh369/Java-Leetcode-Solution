e3f349280f170ab016f091188847f794e946507a
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-11-05 03:10:10 +0530

    Time: 12 ms (13.83%), Space: 43.9 MB (28.27%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -10,3 +10,20 @@ class Solution {
 			.toArray();
     }
 }
+/*
+custom sort explanation:
+.stream(nums)
+iterates through the nums array
+
+.boxed()
+converts each int to Integer object, this is because .sorted() can only operate on objects
+
+.sorted((a,b) -> map.get(a) != map.get(b) ? map.get(a) - map.get(b) : b - a)
+if frequency of two numbers are not the same, sort by ascending frequency. If frequencies are the same, sort by decending numeric value
+
+.mapToInt(n -> n)
+converts Integer to int
+
+.toArray()
+returns array
+*/
