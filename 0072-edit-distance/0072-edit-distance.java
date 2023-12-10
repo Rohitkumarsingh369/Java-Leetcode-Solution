15740eebb4aeedb3f08be5cf6f28279317346f3a
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2023-12-10 16:21:00 +0530

    Time: 4 ms (77.22%), Space: 42.2 MB (98.11%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int minDistance(String word1, String word2) {
+    /*public int minDistance(String word1, String word2) {
          int l1 = word1.length();
         int l2 = word2.length();
         int[][] dp = new int[l1+1][l2+1];
@@ -25,5 +25,31 @@ class Solution {
         }
         
         return dp[l1][l2];
+    }*/
+    public int minDistance(String a,String b){
+        int n=a.length();
+        int m=b.length();
+
+        int dp[]=new int[m+1];
+        
+        for(int i=0;i<=m;i++){
+            dp[i]=i;
+        }
+
+        for(int i=1;i<n+1;i++){
+            int prev=dp[0];
+            dp[0]=i;
+            for(int j=1;j<m+1;j++){
+                int temp=dp[j];
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    dp[j]=prev;
+                }
+                else{
+                    dp[j]=1+Math.min(prev,Math.min(dp[j],dp[j-1]));
+                }
+                prev=temp;
+            }
+        }
+        return dp[m];
     }
 }
