edca932c0bb19a1712f56d5dcaeea7c17fffa13c
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-03-16 21:06:38 +0530

    Time: 1 ms (99.50%), Space: 56.9 MB (68.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,41 +1,21 @@
 class Solution {
-    public int maxSubArray(int[] a) {
-        /*int max_so_far=Integer.MIN_VALUE;
-        int n=a.length;
-		int max_ending_here=0;
-		int start=0,end=0,s=0;
-		for(int i=0;i<n;i++) {
-			max_ending_here=max_ending_here+a[i];
-			if(max_so_far<max_ending_here)
-			{
-				max_so_far=max_ending_here;
-				start=s;
-				end=i;
-			}
-			if(max_ending_here<0)
-			{
-				max_ending_here=0;
-				s=i+1;
-			}
-		}
-        return max_so_far;*/
-        int n=a.length;
+    public int maxSubArray(int[] nums) {
+       
         int maxSoFar=Integer.MIN_VALUE;
         int maxEndingHere=0;
-        int start=0,end=0,s=0;
-        
-        for(int i=0;i<n;i++){
-            maxEndingHere+=a[i];
+        int start=0,end=0,tempStart=0;
+        for(int i=0;i<nums.length;i++){
+            maxEndingHere+=nums[i];
             
             if(maxSoFar<maxEndingHere){
                 maxSoFar=maxEndingHere;
+                start=tempStart;
                 end=i;
-                start=s;
-                    
             }
+            
             if(maxEndingHere<0){
                 maxEndingHere=0;
-                s=i+1;
+                tempStart=i+1;
             }
         }
         return maxSoFar;
