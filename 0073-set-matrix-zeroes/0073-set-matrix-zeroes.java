44e663842132aafa1d7e99cb7be0f8f17f6224d5
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-03-17 13:58:39 +0530

    Time: 1 ms (74.71%), Space: 45.5 MB (62.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -19,21 +19,13 @@ class Solution {
             }
         }
         
-        for(int i=1;i<m;i++){
-            if(matrix[i][0]==0){
-                for(int j=1;j<n;j++){
-                    matrix[i][j]=0;
-             }
-            }
-        }
-        
-        for(int i=1;i<n;i++){
-            if(matrix[0][i]==0){
-                for(int j=1;j<m;j++){
-                    matrix[j][i]=0;
-             }
-            }
-        }
+       for(int i=1;i<m;i++){
+           for(int j=1;j<n;j++){
+               if(matrix[i][0]==0|| matrix[0][j]==0){
+                   matrix[i][j]=0;
+               }
+           }
+       }
         
         if(row){
             for(int i=1;i<n;i++){
