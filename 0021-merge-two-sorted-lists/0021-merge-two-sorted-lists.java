bf01110000630dcf65ee2eb7a71c0585de275b18
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-03-17 20:42:39 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (73.50%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -16,7 +16,6 @@ class Solution {
         
         while(list1!=null && list2!=null){
             
-            
             if(list1.val<=list2.val){
                 tail.next=list1;
                 list1=list1.next;
@@ -28,6 +27,7 @@ class Solution {
             
             tail=tail.next;
         }
+        
         if(list1==null)
             tail.next=list2;
         if(list2==null)
