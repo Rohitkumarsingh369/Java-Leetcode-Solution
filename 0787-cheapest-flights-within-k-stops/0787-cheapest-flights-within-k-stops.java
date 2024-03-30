8b49bd55751da23a54368c732944e0fbc9bdb122
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-03-31 03:50:31 +0530

    Time: 6 ms (88.20%), Space: 44.4 MB (41.39%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.java
@@ -1,21 +1,74 @@
+class Pair{
+    int first;
+    int second;
+    public Pair(int first,int second){
+        this.first = first;
+        this.second = second;
+    }
+}
+class Tuple {
+    int first, second, third; 
+    Tuple(int first, int second, int third) {
+        this.first = first; 
+        this.second = second;
+        this.third = third; 
+    }
+}
+
 class Solution {
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[][] dp = new int[k+2][n];
-        for(int i=0; i<=k+1; i++){
-            Arrays.fill(dp[i],Integer.MAX_VALUE);    
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        // Create the adjacency list to depict airports and flights in
+        // the form of a graph.
+        ArrayList<ArrayList<Pair>> adj = new ArrayList<>(); 
+        for(int i = 0;i<n;i++) {
+            adj.add(new ArrayList<>()); 
         }
-        //fly from src to scr cost 0
-        for(int i=0; i<=k+1; i++){
-            dp[i][src] = 0;    
+        int m = flights.length; 
+        for(int i = 0;i<m;i++) {
+            adj.get(flights[i][0]).add(new Pair(flights[i][1], flights[i][2])); 
         }
         
-        for(int i=1; i<=k+1; i++){
-            for(int[] f: flights){
-                if(dp[i-1][f[0]]!=Integer.MAX_VALUE){
-                    dp[i][f[1]] = Math.min(dp[i][f[1]],dp[i-1][f[0]]+f[2]);
+        // Create a queue which stores the node and their distances from the
+        // source in the form of {stops, {node, dist}} with ‘stops’ indicating 
+        // the no. of nodes between src and current node.
+        Queue<Tuple> q = new LinkedList<>(); 
+        
+        q.add(new Tuple(0, src, 0));
+
+        // Distance array to store the updated distances from the source. 
+        int[] dist = new int[n]; 
+        for(int i = 0;i<n;i++) {
+            dist[i] = (int)(1e9); 
+        }
+        dist[src] = 0; 
+
+        // Iterate through the graph using a queue like in Dijkstra with 
+        // popping out the element with min stops first.
+        while(!q.isEmpty()) {
+            Tuple it = q.peek(); 
+            q.remove(); 
+            int stops = it.first; 
+            int node = it.second; 
+            int cost = it.third; 
+            
+            // We stop the process as soon as the limit for the stops reaches.
+            if(stops > K) continue; 
+            for(Pair iter: adj.get(node)) {
+                int adjNode = iter.first; 
+                int edW = iter.second; 
+                
+                // We only update the queue if the new calculated dist is
+                // less than the prev and the stops are also within limits.
+                if (cost + edW < dist[adjNode] && stops <= K) {
+                    dist[adjNode] = cost + edW; 
+                    q.add(new Tuple(stops + 1, adjNode, cost + edW)); 
                 }
             }
         }
-        return dp[k+1][dst] == Integer.MAX_VALUE ? -1 : dp[k+1][dst];
+        // If the destination node is unreachable return ‘-1’
+        // else return the calculated dist from src to dst.
+        if(dist[dst] == (int)(1e9)) return -1; 
+        return dist[dst]; 
+                      
     }
 }
