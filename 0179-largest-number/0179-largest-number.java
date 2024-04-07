3da5f4ed5f38cee434ff2b84a75b108e3ba56399
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-04-07 23:51:12 +0530

    Time: 5 ms (96.89%), Space: 43.8 MB (29.82%) - LeetHub

--- 0179-largest-number/0179-largest-number.java
+++ 0179-largest-number/0179-largest-number.java
@@ -1,36 +1,35 @@
 class Solution {
-    public String largestNumber(int[] nums) {
-        String[] strs = new String[nums.length];
-    for(int i=0; i<nums.length; i++){
-        strs[i] = String.valueOf(nums[i]);
-    }
- 
-    Arrays.sort(strs, new Comparator<String>(){
-        public int compare(String s1, String s2){
-            String leftRight = s1+s2;
-            String rightLeft = s2+s1;
-            return -leftRight.compareTo(rightLeft);
- 
+    private class LargerNumberComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+           return order2.compareTo(order1);
         }
-    });
- 
-    StringBuilder sb = new StringBuilder();
-    for(String s: strs){
-        sb.append(s);
     }
- 
-    while(sb.charAt(0)=='0' && sb.length()>1){
-        sb.deleteCharAt(0);
-    }
- 
-    return sb.toString();
+
+    public String largestNumber(int[] nums) {
+        // Get input integers as strings.
+        String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
+        }
+
+        // Sort strings according to custom comparator.
+        Arrays.sort(asStrs, new LargerNumberComparator());
+
+        // If, after being sorted, the largest number is `0`, the entire number
+        // is zero.
+        if (asStrs[0].equals("0")) {
+            return "0";
+        }
+
+        // Build largest number from sorted array.
+        String largestNumberStr = new String();
+        for (String numAsStr : asStrs) {
+            largestNumberStr += numAsStr;
+        }
+
+        return largestNumberStr;
     }
 }
-
-/*
-public static String largestNumber(int[] nums) {
-  return Arrays.stream(nums)
-      .mapToObj(p->String.valueOf(p))
-      .sorted((s1, s2)->(s2+s1).compareTo(s1+s2))
-      .collect(Collectors.joining(""));
-}*/
