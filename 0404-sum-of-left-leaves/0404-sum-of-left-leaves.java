6fde4801689f01f7be30744d45a2ac3c0e533621
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-08-20 12:11:09 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (9.60%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.java
@@ -15,28 +15,20 @@
  */
 class Solution {
     public int sumOfLeftLeaves(TreeNode root) {
-        
-        Stack<TreeNode> stack=new Stack<>();
-        
-        stack.push(root);
-        
-        int sum=0;
-
-        while(!stack.isEmpty()){
-            TreeNode current=stack.pop();
-            
-            if(current.left!=null){        
-                if(current.left.left==null && current.left.right==null)
-                    sum+=current.left.val;
-                
-                else
-                    stack.push(current.left);
-            }
-            if(current.right!=null){
-                stack.push(current.right);
-            }
+        return sumLeftLeaves(root, false);
+    }
+    private int sumLeftLeaves(TreeNode root,boolean isLeft){
+        if(root==null){
+            return 0;
         }
         
-        return sum;
+        if(root.left==null && root.right==null && isLeft){
+            return root.val;
+        }
+        
+        int leftSum=sumLeftLeaves(root.left, true);
+        int rightSum=sumLeftLeaves(root.right, false);
+        
+        return leftSum+rightSum;
     }
 }
