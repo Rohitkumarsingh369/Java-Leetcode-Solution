1fc3186eedf00f1402a3e0b23cb2437b65e8538a
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-08-24 16:42:35 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (17.31%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,17 @@
 class Solution {
     public int climbStairs(int n) {
-    // base cases
-    if(n <= 0) return 0;
-    if(n == 1) return 1;
-    if(n == 2) return 2;
-    
-    int one_step_before = 2;
-    int two_steps_before = 1;
-    int all_ways = 0;
-    
-    for(int i=2; i<n; i++){
-    	all_ways = one_step_before + two_steps_before;
-    	two_steps_before = one_step_before;
-        one_step_before = all_ways;
-    }
-    return all_ways;
+        if(n==0 || n==1){
+            return 1;
+        }
+        
+        int step1=1;
+        int step2=1;
+        
+        for(int i=2;i<=n;i++){
+            int step=step1+step2;
+            step1=step2;
+            step2=step;
+        }
+        return step2;
     }
 }
