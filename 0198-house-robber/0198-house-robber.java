a9ca89a976930098957402279ce3e880f2ebf67c
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-08-25 11:50:55 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (32.01%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,42 +1,23 @@
 class Solution {
-    public int rob(int[] hval) {
-        /*int n=hval.length;
-        if (n == 0)
-        return 0;
-        if (n == 1)
-            return hval[0];
-        if (n == 2)
-            return Math.max(hval[0], hval[1]);
-   
-        // dp[i] represent the maximum value stolen
-        // so far after reaching house i.
-        int[] dp = new int[n];
-   
-        // Initialize the dp[0] and dp[1]
-        dp[0] = hval[0];
-        dp[1] = Math.max(hval[0], hval[1]);
-   
-        // Fill remaining positions
-        for (int i = 2; i<n; i++)
-            dp[i] = Math.max(hval[i]+dp[i-2], dp[i-1]);
-   
-        return dp[n-1];*/
+    public int rob(int[] nums) {
+        int len=nums.length;
+        int prev2=0,prev=nums[0];
         
-        
-        int length=hval.length;
-        if(length==0)
-            return 0;
-        if(length==1)
-            return hval[0];
-        if(length==2)
-            return Math.max(hval[0],hval[1]);
-        
-        hval[1]=Math.max(hval[0],hval[1]);
-        
-        for(int i=2;i<length;i++){
-            hval[i]=Math.max(hval[i-1],hval[i]+hval[i-2]);
+        for(int i=1;i<len;i++){
+            int pick=nums[i];
+            
+            if(i>1){
+                pick+=prev2;
+            }
+            
+            int notPick=prev;
+            
+            int current=Math.max(pick,notPick);
+            
+            prev2=prev;
+            prev=current;
         }
         
-        return hval[length-1];
+        return prev;
     }
 }
