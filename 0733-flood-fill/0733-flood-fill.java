e9051c65759cfb5e9229f81f5c8969c0ef0971c7
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-08-27 18:10:44 +0530

    Time: 1 ms (45.28%), Space: 44.5 MB (92.39%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,34 +1,36 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int oldColor = image[sr][sc];
-        if (oldColor == newColor) return image; // No need to proceed if colors are the same
-
-        int rows = image.length, cols = image[0].length;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{sr, sc});
-
-        image[sr][sc] = newColor; // Start filling with new color
-
-        // Direction vectors for moving up, down, left, right
-        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        while (!queue.isEmpty()) {
-            int[] pixel = queue.poll();
-            int r = pixel[0], c = pixel[1];
-
-            // Check all 4 directions
-            for (int[] direction : directions) {
-                int nr = r + direction[0];
-                int nc = c + direction[1];
-
-                // Proceed if within bounds and matches old color
-                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && image[nr][nc] == oldColor) {
-                    queue.offer(new int[]{nr, nc});
-                    image[nr][nc] = newColor; // Fill with new color
+       int oldColor=image[sr][sc];
+        
+        if(oldColor==newColor){
+            return image;
+        }
+        
+        int rows=image.length;
+        int cols=image[0].length;
+        
+        Queue<int[]> queue=new LinkedList<>();
+        
+        queue.offer(new int[]{sr,sc});
+        image[sr][sc]=newColor;
+        
+        int [][] directions={{0,1},{1,0},{0,-1},{-1,0}};
+        while(!queue.isEmpty()){
+            int [] pixel=queue.poll();
+            
+            int row=pixel[0];
+            int col=pixel[1];
+            
+            for(int [] direction :directions){
+                int nr=row+direction[0];
+                int nc=col+direction[1];
+                
+                if(nr>=0 && nr<rows && nc>=0 && nc<cols && image[nr][nc]==oldColor){
+                    image[nr][nc]=newColor;
+                    queue.offer(new int[]{nr,nc});
                 }
             }
         }
-
         return image;
     }
 }
