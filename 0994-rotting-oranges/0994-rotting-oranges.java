0680a20a561acbdb33732ee7fbc3031869a0a5f5
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-08-27 12:06:45 +0530

    Time: 1 ms (100.00%), Space: 42.6 MB (30.58%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        if(grid==null || grid.length==0){
+            return 0;
+        }
+        
+        int rows=grid.length;
+        int cols=grid[0].length;
+        
+        int count_fresh=0;
+        
+        
+        Queue<int []> queue=new LinkedList<>();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==2){
+                    queue.offer(new int[]{i,j});
+                }
+                if(grid[i][j]==1){
+                    count_fresh++;
+                }
+            }
+        }
+        //System.out.println(count_fresh);
+        if(count_fresh==0){
+            return 0;
+        }
+        
+        int dx[]={0,0,1,-1};
+        int dy[]={1,-1,0,0};
+        
+        int count_min=0,count=0;
+        
+        while(!queue.isEmpty()){
+            int size=queue.size();
+           // count+=size;
+            
+            for(int i=0;i<size;i++){
+                int point[]=queue.poll();
+                
+                for(int j=0;j<4;j++){
+                    int x=point[0]+dx[j];
+                    int y=point[1]+dy[j];
+                    
+                    if(x<0||y<0||x>=rows||y>=cols||grid[x][y]==2||grid[x][y]==0)
+                        continue;
+                    
+                    grid[x][y]=2;
+                    queue.offer(new int[]{x,y});
+                    count_fresh--;
+                }
+            }
+            if(queue.size()!=0){
+                count_min++;
+            }
+        }
+        
+        return count_fresh==0?count_min:-1;
+    }
+}
