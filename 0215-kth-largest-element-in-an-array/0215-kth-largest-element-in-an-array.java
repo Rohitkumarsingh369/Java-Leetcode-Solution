3cfa2dfe1a39722106478abbb25676cd0bbf77ed
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-01 10:57:31 +0530

    Time: 60 ms (49.73%), Space: 59.5 MB (31.23%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,55 +1,15 @@
 class Solution {
-   /* public int findKthLargest(int[] nums, int k) {  
-        return findKthLargest(nums, 0, nums.length-1, k-1);  
-    }  
-  
-    private int findKthLargest(int[] nums, int left, int right, int k) {  
-        if(k<left || k>right) return -1; 
-        while(true){
-             
-            int index = partition(nums, left, right);  
-           
-            if(index == k) {  
-                return nums[index]; 
-            }
-            if(index>k){
-                right=index-1;
-            }
-            else{
-                left=index+1;
-            }
-        }
-         
-    }  
-
-    private int partition(int[] nums, int l, int h) { 
-        int pivot = nums[h];  
-        int p = l;  
-        for(int i=l; i<h; i++) {  
-            if(nums[i]>pivot) {  
-                swap(nums, i, p++);  
-            }  
-        }  
-        swap(nums, p, h);  
-        return p;  
-    }  
-
-    private void swap(int[] nums, int i, int j) {  
-        int tmp = nums[i];  
-        nums[i] = nums[j];  
-        nums[j] = tmp;  
-    }*/
     public int findKthLargest(int[] nums, int k) {
-
-    final PriorityQueue<Integer> pq = new PriorityQueue<>();
-    for(int val : nums) {
-        pq.offer(val);
-
-        System.out.println(pq.peek());
-        if(pq.size() > k) {
-            pq.poll();
+        PriorityQueue<Integer> minHeap=new PriorityQueue<>(k);
+        
+        for(int num:nums){
+            minHeap.add(num);
+            
+            if(minHeap.size()>k){
+                minHeap.poll();
+            }
         }
+        
+        return minHeap.peek();
     }
-    return pq.peek();
-}
 }
