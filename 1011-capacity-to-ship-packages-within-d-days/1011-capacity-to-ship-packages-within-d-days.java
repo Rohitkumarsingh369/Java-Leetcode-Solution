8a7dab0ec26af47fdc84901d62990a42e816d267
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-04 18:54:26 +0530

    Time: 12 ms (31.43%), Space: 47.1 MB (48.09%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,38 +1,37 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int d) {
-         //Find the maximum and the summation:
-        int low = Integer.MIN_VALUE, high = 0;
-        for (int i = 0; i < weights.length; i++) {
-            high += weights[i];
-            low = Math.max(low, weights[i]);
+    public int shipWithinDays(int[] weights, int days) {
+        
+        int low=Integer.MIN_VALUE,high=0;
+        
+        for(int i=0;i<weights.length;i++){
+            low=Math.max(low,weights[i]);
+            high+=weights[i];
         }
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int numberOfDays = findDays(weights, mid);
-            if (numberOfDays <= d) {
-                //eliminate right half
-                high = mid - 1;
-            } else {
-                //eliminate left half
-                low = mid + 1;
+        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            int numberOfDays=findDays(weights,mid);
+            if(numberOfDays<=days){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
         return low;
     }
-    public int findDays(int[] weights, int cap) {
-        int days = 1; //First day.
-        int load = 0;
-        int n = weights.length; //size of array.
-        for (int i = 0; i < n; i++) {
-            if (load + weights[i] > cap) {
-                days += 1; //move to next day
-                load = weights[i]; //load the weight.
-            } else {
-                //load the weight on the same day.
-                load += weights[i];
+    public int findDays(int []weights, int cap){
+        int day=1,load=0;
+        for(int i=0;i<weights.length;i++){
+            if(load+weights[i]>cap){
+                day++;
+                load=weights[i];
+            }
+            else{
+                load+=weights[i];
             }
         }
-        return days;
+        return day;
     }
 }
