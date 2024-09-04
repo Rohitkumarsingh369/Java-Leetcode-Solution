d6ddf5e96a0493700d0a9116259aac869dcb4584
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-04 20:36:28 +0530

    Time: 4 ms (49.00%), Space: 85.4 MB (7.64%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,18 +1,29 @@
 class Solution {
-    public int[] rearrangeArray(int[] a) {
-        int n=a.length;
-        int result[]=new int[n];
-        int pos=0,neg=1;
-        for(int i=0;i<n;i++){
-            if(a[i]>0){
-                result[pos]=a[i];
-                pos=pos+2;
-            }
-            else{
-                result[neg]=a[i];
-                neg=neg+2;
+    public int[] rearrangeArray(int[] nums) {
+        // Step 1: Separate positive and negative numbers
+        int n = nums.length;
+        int[] pos = new int[n / 2];
+        int[] neg = new int[n / 2];
+        
+        int posIndex = 0, negIndex = 0;
+        
+        for (int num : nums) {
+            if (num > 0) {
+                pos[posIndex++] = num;
+            } else {
+                neg[negIndex++] = num;
             }
         }
+        
+        // Step 2: Interleave positive and negative numbers
+        int[] result = new int[n];
+        int index = 0;
+        
+        for (int i = 0; i < n / 2; i++) {
+            result[index++] = pos[i];
+            result[index++] = neg[i];
+        }
+        
         return result;
     }
 }
