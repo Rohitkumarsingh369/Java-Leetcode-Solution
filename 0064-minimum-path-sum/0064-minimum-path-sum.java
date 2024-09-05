31cdba8379b636f2ab90b263900d31bfa2e20fa7
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-06 04:43:25 +0530

    Time: 3 ms (63.47%), Space: 47.7 MB (7.46%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -19,23 +19,30 @@ class Solution {
         
         return grid[row-1][col-1];*/
         
-        int row=grid.length;
-        int col=grid[0].length;
+        int rows = grid.length;
+        int cols = grid[0].length;
         
-        int []dp=new int[col];
-            dp[0]=grid[0][0];
-        for(int i=1;i<col;i++){
-            dp[i]=dp[i-1]+grid[0][i];
+        // Initialize a 1D dp array for storing the minimum path sums for each column
+        int[] dp = new int[cols];
+        
+        // Fill the dp array for the first row
+        dp[0] = grid[0][0];
+        for (int j = 1; j < cols; j++) {
+            dp[j] = dp[j - 1] + grid[0][j];
         }
         
-        for(int i=1;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(j==0)
-                    dp[j]+=grid[i][j];
-                else if(j>0)
-                    dp[j]=grid[i][j]+Math.min(dp[j],dp[j-1]);
+        // Iterate over the grid starting from the second row
+        for (int i = 1; i < rows; i++) {
+            // Update the first element in the current row
+            dp[0] += grid[i][0];
+            
+            // Update the rest of the dp array for the current row
+            for (int j = 1; j < cols; j++) {
+                dp[j] = grid[i][j] + Math.min(dp[j], dp[j - 1]);
             }
         }
-        return dp[col-1];
+        
+        // The last element in dp contains the minimum path sum to the bottom-right corner
+        return dp[cols - 1];
     }
 }
