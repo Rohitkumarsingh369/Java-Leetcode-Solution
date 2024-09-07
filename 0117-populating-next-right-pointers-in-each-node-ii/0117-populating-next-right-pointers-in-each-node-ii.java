add4cd795a429686b2573f8b7990dbe41a507a64
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-08 04:08:53 +0530

    Time: 1 ms (74.15%), Space: 44 MB (83.37%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -0,0 +1,65 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+         if (root == null) {
+            return null;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            Node prev = null;
+
+            for (int i = 0; i < size; i++) {
+                Node current = queue.poll();
+
+                // Connect the previous node's next pointer to the current node
+                if (prev != null) {
+                    prev.next = current;
+                }
+
+                // Update previous node to the current node
+                prev = current;
+
+                // Add left and right children to the queue
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+
+            // The last node of each level should point to null
+            if (prev != null) {
+                prev.next = null;
+            }
+        }
+
+        return root;
+    }
+}
