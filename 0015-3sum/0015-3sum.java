e632aa9c999e202d14cdfb961b6137b844a4dd8e
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-07 22:34:35 +0530

    Time: 30 ms (67.13%), Space: 51.5 MB (58.40%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,42 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result=new ArrayList<>();
-        
-        if(nums==null || nums.length<3){
-            return result;
-        }
-        
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-2;i++){
-            if(i-1>=0 && nums[i]==nums[i-1]){
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums); // Sort the array
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            // Skip duplicates for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            int left=i+1;
-            int right=nums.length-1;
-            
-            while(left<right){
-                int sum=nums[i]+nums[left]+nums[right];
-                if(sum==0){
-                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
-                    
-                    while(left+1<right && nums[left]==nums[left+1]){
+
+            int left = i + 1; // Start pointer for the two-pointer technique
+            int right = nums.length - 1; // End pointer for the two-pointer technique
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // Skip duplicates for the second and third elements
+                    while (left < right && nums[left] == nums[left + 1]) {
                         left++;
                     }
-                    
-                    while(left<right-1 && nums[right]==nums[right-1]){
+                    while (left < right && nums[right] == nums[right - 1]) {
                         right--;
                     }
-                    
+
                     left++;
                     right--;
-                    
+                } else if (sum < 0) {
+                    left++; // Increase the sum
+                } else {
+                    right--; // Decrease the sum
                 }
-                else if(sum<0)
-                    left++;
-                else
-                    right--;
             }
         }
+
         return result;
     }
 }
