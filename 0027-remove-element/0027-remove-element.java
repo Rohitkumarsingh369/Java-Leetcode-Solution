f65e2bbf178bc25e0ec063be22c8a78515e80278
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-08 09:14:29 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (70.23%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,13 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i=0,j=0;
-        
-        for(;j<nums.length;j++){
-            if(nums[j]!=val){
-                nums[i]=nums[j];
-                i++;
+        int i = 0;  // Pointer to track the position for the next element that is not equal to val
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];  // Assign current element to the position tracked by i
+                i++;  // Move the pointer i
             }
         }
+
         return i;
     }
 }
