425b8480e2ab8ef47292bdbb510491488c5bd738
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-08 11:34:52 +0530

    Time: 134 ms (78.64%), Space: 41.2 MB (96.30%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,11 +1,15 @@
 class Solution {
-    static boolean[][] visited;
     public boolean exist(char[][] board, String word) {
-        visited = new boolean[board.length][board[0].length];
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
         
-        for(int i = 0; i < board.length; i++){
-            for(int j = 0; j < board[i].length; j++){
-                if((word.charAt(0) == board[i][j]) && search(board, word, i, j, 0)){
+        int rows = board.length;
+        int cols = board[0].length;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (dfs(board, word, i, j, 0)) {
                     return true;
                 }
             }
@@ -14,24 +18,28 @@ class Solution {
         return false;
     }
     
-    private boolean search(char[][]board, String word, int i, int j, int index){
-        if(index == word.length()){
-            return true;
+    private boolean dfs(char[][] board, String word, int i, int j, int index) {
+        if (index == word.length()) {
+            return true; // Found the word
         }
         
-        if(i >= board.length || i < 0 || j >= board[i].length || j < 0 || board[i][j] != word.charAt(index) || visited[i][j]){
-            return false;
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index)) {
+            return false; // Out of bounds or mismatch
         }
         
-        visited[i][j] = true;
-        if(search(board, word, i-1, j, index+1) || 
-           search(board, word, i+1, j, index+1) ||
-           search(board, word, i, j-1, index+1) || 
-           search(board, word, i, j+1, index+1)){
-            return true;
-        }
+        // Temporarily mark the cell as visited
+        char temp = board[i][j];
+        board[i][j] = '#'; 
         
-        visited[i][j] = false;
-        return false;
+        // Explore all possible directions (right, left, down, up)
+        boolean found = dfs(board, word, i + 1, j, index + 1) ||
+                        dfs(board, word, i - 1, j, index + 1) ||
+                        dfs(board, word, i, j + 1, index + 1) ||
+                        dfs(board, word, i, j - 1, index + 1);
+        
+        // Unmark the cell (backtrack)
+        board[i][j] = temp;
+        
+        return found;
     }
 }
