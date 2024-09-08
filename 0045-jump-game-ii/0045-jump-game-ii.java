4f96579fefecf459779ffe3bd80c2ebecc8aefb1
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-08 15:31:40 +0530

    Time: 2 ms (59.67%), Space: 45.1 MB (51.87%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,21 +1,24 @@
 class Solution {
-   
     public int jump(int[] nums) {
-        
-        int jump=0,curend=0,maxlocation=0;
-        
-        for(int i=0;i<nums.length-1;i++){
-            
-            maxlocation=Math.max(maxlocation,i+nums[i]);
-            
-            if(i==curend){
-                jump++;
-                curend=maxlocation;
+         int n = nums.length;
+        if (n <= 1) return 0;
+
+        int jumps = 0;
+        int currentEnd = 0;
+        int farthest = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+
+            if (i == currentEnd) {
+                jumps++;
+                currentEnd = farthest;
+                if (currentEnd >= n - 1) {
+                    break;
+                }
             }
         }
-        
-        return jump;
- }
-}
 
-/*The main idea is based on greedy. Let's say the range of the current jump is [curBegin, curEnd], curFarthest is the farthest point that all points in [curBegin, curEnd] can reach. Once the current point reaches curEnd, then trigger another jump, and set the new curEnd with curFarthest, then keep the above steps, as the following:*/
+        return jumps;
+    }
+}
