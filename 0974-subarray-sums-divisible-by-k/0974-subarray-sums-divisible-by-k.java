03508e7d1fb4f0832f8c975199daaeac5409d8e1
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-24 14:44:48 +0530

    Time: 23 ms (64.66%), Space: 49.3 MB (13.13%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,9 +1,9 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-       // HashMap to store the frequency of prefix sum remainders
+        // HashMap to store the frequency of prefix sum remainders
         HashMap<Integer, Integer> remainderFrequency = new HashMap<>();
         
-        // Initial case: remainder 0 has occurred once (to count subarrays starting from the beginning)
+        // Initial case: remainder 0 has occurred once
         remainderFrequency.put(0, 1);
         
         int prefixSum = 0;
@@ -17,15 +17,13 @@ class Solution {
             // Calculate the remainder of the current prefix sum with k
             int remainder = prefixSum % k;
             
-            // If the remainder is negative, convert it to a positive equivalent in the range [0, k-1]
+            // If the remainder is negative, convert it to positive within range [0, k-1]
             if (remainder < 0) {
                 remainder += k;
             }
             
-            // Check how many times this remainder has occurred before
-            if (remainderFrequency.containsKey(remainder)) {
-                count += remainderFrequency.get(remainder);
-            }
+            // Add the count of subarrays ending at this index
+            count += remainderFrequency.getOrDefault(remainder, 0);
             
             // Update the frequency of this remainder
             remainderFrequency.put(remainder, remainderFrequency.getOrDefault(remainder, 0) + 1);
