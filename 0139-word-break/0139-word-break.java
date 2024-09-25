d44facb071b63a4ea2f81914460beb88e8eead1c
Rohit Kumar Singh <70133502+Rohitkumarsingh369@users.noreply.github.com>
2024-09-25 06:19:43 +0530

    Time: 8 ms (43.39%), Space: 44.1 MB (56.42%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,18 +1,24 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Set<String> wordSet = new HashSet<>(wordDict);  // Convert list to set for faster lookup
+         // Create a set for faster lookup
+        Set<String> wordSet = new HashSet<>(wordDict);
+        
+        // Create a DP array, dp[i] indicates if s[0:i] can be segmented
         boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
-
+        dp[0] = true; // Base case: empty string can always be segmented
+        
+        // Fill the DP array
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 0; j < i; j++) {
+                // If s[0:j] can be segmented and s[j:i] is in wordDict, set dp[i] to true
                 if (dp[j] && wordSet.contains(s.substring(j, i))) {
                     dp[i] = true;
-                    break;
+                    break; // No need to check further if dp[i] is already true
                 }
             }
         }
-
+        
+        // Return the result for the full string
         return dp[s.length()];
     }
 }
